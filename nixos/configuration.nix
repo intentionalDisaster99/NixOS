772472c248e7ca03aa065cb92fbbf64f99f24eb3
Sa9m <abyssalflerken@gmail.com>
2025-09-21 18:37:50 -0500

    New configuration!

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -165,6 +165,7 @@
     cd = "z";
     nrs = "/etc/nixos/scripts/nrs.sh";
     windows = "sudo grub-reboot 1 && reboot";
+    pioStart = "nix run --impure github:xdadrm/nixos_use_platformio_patformio-ide_and_vscode#codium --";
   };
 
   # Showing off my nerdiness
