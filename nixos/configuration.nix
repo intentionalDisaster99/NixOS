23a05bc3021809309ec7902b5c92cca57a8f76e9
Sa9m <abyssalflerken@gmail.com>
2025-09-21 17:31:23 -0500

    New configuration!

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -294,6 +294,13 @@
     ];
   };    
 
+  {
+  services.udev.packages = [ 
+    pkgs.platformio-core
+    pkgs.openocd
+  ];
+}
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
