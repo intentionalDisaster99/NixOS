52ba96067a197eefd549da353f0abc3b2223b3d0
Sa9m <abyssalflerken@gmail.com>
2025-09-26 16:56:21 -0500

    New configuration!

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,10 +6,6 @@
 
   systemd.user.startServices = "sd-switch";
 
-  home.packages = [
-    pkgs.rclone
-  ];
-
   
   # Enabling Programs
   # programs.waybar.enable = true;
@@ -28,41 +24,6 @@
   home.file.".config/hypr/hyprland.conf".source = ../modules/hypr/hyprland.conf;
   home.file.".config/waybar/config".source = ../modules/hypr/waybar/config.jsonc;
   home.file.".config/waybar/style.css".source = ../modules/hypr/waybar/style.css;
-
-  #######################
-  ### Systemd Services###
-  #######################
-
-  # Rclone google drive
-  systemd.user.services.rclone-gdrive = {
-    # This block defines the service's metadata and dependencies
-    Unit = {
-      Description = "Rclone mount for GDrive";
-      # These are now correctly placed inside the Unit block
-      Wants = [ "network-online.target" ];
-      After = [ "network-online.target" ];
-    };
-
-    # This block defines the commands to run
-    Service = {
-      ExecStart = ''
-        ${pkgs.rclone}/bin/rclone mount GDrive: /home/sa9m/GDrive \
-          --vfs-cache-mode writes \
-          --config=${config.home.homeDirectory}/.config/rclone/rclone.conf
-      '';
-      ExecStop = "${pkgs.fusermount}/bin/fusermount -u /home/sa9m/GDrive";
-      Restart = "on-failure";
-      RestartSec = 10;
-      Type = "simple";
-    };
-
-    # This block tells systemd when to start the service
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
-
-
 
 
   home.stateVersion = "23.11";
