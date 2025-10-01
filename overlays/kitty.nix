85f1ce9b86547f507ec421d1753e44a3e94d5068
Sa9m <abyssalflerken@gmail.com>
2025-09-30 23:15:47 -0500

    Update NixOS configuration on Tue Sep 30 23:15:47 CDT 2025

--- overlays/kitty.nix
+++ overlays/kitty.nix
@@ -2,10 +2,7 @@
 final: prev:
 
 {
-  # We are overriding the 'kitty' package.
   kitty = prev.kitty.overrideAttrs (oldAttrs: {
-    # This is a more aggressive way to skip tests by replacing
-    # the entire phase with a command that does nothing.
     installCheckPhase = ''
       echo "Skipping failing kitty tests."
     '';
