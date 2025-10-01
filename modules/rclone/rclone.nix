6ca6c9c08855b0028c994aebd7331bad6f8f41f4
Sa9m <abyssalflerken@gmail.com>
2025-10-01 14:51:42 -0500

    Update NixOS configuration on Wed Oct  1 14:51:42 CDT 2025

--- modules/rclone/rclone.nix
+++ modules/rclone/rclone.nix
@@ -4,19 +4,34 @@
   environment.systemPackages = [ pkgs.rclone ];
 
   environment.etc."rclone.conf".text = ''
-        # Google Drive
-        [gdrive]
+    # Google Drive
+    [gdrive]
       type: drive
-    client_id: 935963070500-1s0tcq7a52khcpmrd88f451mu9g28gvq.apps.googleusercontent.com
-    client_secret: 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
-    scope: drive
-    token: {"access_token":"ya29.a0AQQ_BDRZEgh50jmreFJsaNtxkxu3Fu3rGIaEME-T9l1g9UjED0IKNDty09WE_yHb_eMm_65tJmhjyDZmzWIRNxbdtewAxUusfBmjAfD5dSiJcaxEUD9gyXF3O-8smOS7envRPJzrgqqIef24PUa9-ppvfSds4fOvspcZpyuhdZUqcrheqH97EUQcWSgIKY8WxhBfzHIaCgYKAcUSARcSFQHGX2MiGlx1Uwz9FEuiiB7pIOSv3A0206","token_type":"Bearer","refresh_token":"1//01YcyVzYn3eIACgYIARAAGAESNwF-L9IrmlqQeObfFLEGwrVntjDiZ7Uldw2UM4p-IiBoVT3I0tWsoKFj1IqqeGRxnz_wWtTCs4I","expiry":"2025-10-01T14:05:20.364781868-05:00","expires_in":3599}
-    
-        # my Nas
-        [NAS]
-        type: smb
-    host: graviton
-    pass: LD6JWri2w-Hqy81goeYRu5-e1m0Cc2esUQ
+      client_id: 935963070500-1s0tcq7a52khcpmrd88f451mu9g28gvq.apps.googleusercontent.com
+      client_secret: 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
+      scope: drive
+      token: {"access_token":"ya29.a0AQQ_BDRZEgh50jmreFJsaNtxkxu3Fu3rGIaEME-T9l1g9UjED0IKNDty09WE_yHb_eMm_65tJmhjyDZmzWIRNxbdtewAxUusfBmjAfD5dSiJcaxEUD9gyXF3O-8smOS7envRPJzrgqqIef24PUa9-ppvfSds4fOvspcZpyuhdZUqcrheqH97EUQcWSgIKY8WxhBfzHIaCgYKAcUSARcSFQHGX2MiGlx1Uwz9FEuiiB7pIOSv3A0206","token_type":"Bearer","refresh_token":"1//01YcyVzYn3eIACgYIARAAGAESNwF-L9IrmlqQeObfFLEGwrVntjDiZ7Uldw2UM4p-IiBoVT3I0tWsoKFj1IqqeGRxnz_wWtTCs4I","expiry":"2025-10-01T14:05:20.364781868-05:00","expires_in":3599}
+
+    [Atlantis]
+      type: drive
+      client_id: 935963070500-935963070500-1s0tcq7a52khcpmrd88f451mu9g28gvq.apps.googleusercontent.com
+      client_secret: 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
+      scope: drive
+      token: {"access_token":"ya29.a0AQQ_BDRZEgh50jmreFJsaNtxkxu3Fu3rGIaEME-T9l1g9UjED0IKNDty09WE_yHb_eMm_65tJmhjyDZmzWIRNxbdtewAxUusfBmjAfD5dSiJcaxEUD9gyXF3O-8smOS7envRPJzrgqqIef24PUa9-ppvfSds4fOvspcZpyuhdZUqcrheqH97EUQcWSgIKY8WxhBfzHIaCgYKAcUSARcSFQHGX2MiGlx1Uwz9FEuiiB7pIOSv3A0206","token_type":"Bearer","refresh_token":"1//01YcyVzYn3eIACgYIARAAGAESNwF-L9IrmlqQeObfFLEGwrVntjDiZ7Uldw2UM4p-IiBoVT3I0tWsoKFj1IqqeGRxnz_wWtTCs4I","expiry":"2025-10-01T14:05:20.364781868-05:00","expires_in":3599}
+
+    [Havoc]
+      type: drive
+      client_id: 935963070500-935963070500-1s0tcq7a52khcpmrd88f451mu9g28gvq.apps.googleusercontent.com
+      client_secret: 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
+      scope: drive
+      token: {"access_token":"ya29.a0AQQ_BDRZEgh50jmreFJsaNtxkxu3Fu3rGIaEME-T9l1g9UjED0IKNDty09WE_yHb_eMm_65tJmhjyDZmzWIRNxbdtewAxUusfBmjAfD5dSiJcaxEUD9gyXF3O-8smOS7envRPJzrgqqIef24PUa9-ppvfSds4fOvspcZpyuhdZUqcrheqH97EUQcWSgIKY8WxhBfzHIaCgYKAcUSARcSFQHGX2MiGlx1Uwz9FEuiiB7pIOSv3A0206","token_type":"Bearer","refresh_token":"1//01YcyVzYn3eIACgYIARAAGAESNwF-L9IrmlqQeObfFLEGwrVntjDiZ7Uldw2UM4p-IiBoVT3I0tWsoKFj1IqqeGRxnz_wWtTCs4I","expiry":"2025-10-01T14:05:20.364781868-05:00","expires_in":3599}
+
+
+    # my Nas
+      [NAS]
+      type: smb
+      host: graviton
+      pass: LD6JWri2w-Hqy81goeYRu5-e1m0Cc2esUQ
   '';
 
   # Adding in folders if they aren't already there
@@ -28,6 +43,38 @@
   fileSystems = {
     "/home/sa9m/GDrive/My Drive" = {
       device = "gdrive:/";
+      fsType = "rclone";
+      options = [
+        "allow_other"
+        "nofail"
+        "config=/etc/rclone.conf"
+        "vfs-cache-mode=writes"
+        "vfs-cache-max-age=24h"
+        "vfs-cache-max-size=2G"
+        "uid=sa9m"
+        "gid=users"
+        "no-modtime"
+      ];
+    };
+
+    "/home/sa9m/GDrive/Shared Drives/Atlantis" = {
+      device = "Atlantis:/";
+      fsType = "rclone";
+      options = [
+        "allow_other"
+        "nofail"
+        "config=/etc/rclone.conf"
+        "vfs-cache-mode=writes"
+        "vfs-cache-max-age=24h"
+        "vfs-cache-max-size=2G"
+        "uid=sa9m"
+        "gid=users"
+        "no-modtime"
+      ];
+    };
+
+    "/home/sa9m/GDrive/Shared Drives/Havoc" = {
+      device = "Havoc:/";
       fsType = "rclone";
       options = [
         "allow_other"
