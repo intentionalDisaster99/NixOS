6727714b1cd54d731fa9b845bbdd18e1522ba3bc
Sa9m <abyssalflerken@gmail.com>
2025-09-30 23:13:41 -0500

    Update NixOS configuration on Tue Sep 30 23:13:41 CDT 2025

--- overlays/kitty.nix
+++ overlays/kitty.nix
@@ -1,11 +1,6 @@
 # This overlay disables the failing tests for the kitty terminal package.
-# An overlay is a function that takes two arguments:
-final: prev:
-
 {
   kitty = prev.kitty.overrideAttrs (oldAttrs: {
-    # This is a more aggressive way to skip tests by replacing
-    # the entire phase with a command that does nothing.
     installCheckPhase = ''
       echo "Skipping failing kitty tests."
     '';
