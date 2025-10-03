7be7963245fb0a3952865de048744e03ec46b10e
Sa9m <abyssalflerken@gmail.com>
2025-10-03 00:10:31 -0500

    Update NixOS configuration on Fri Oct  3 00:10:31 CDT 2025

--- modules/drive/drive.nix
+++ modules/drive/drive.nix
@@ -2,14 +2,10 @@
 
 {
   environment.systemPackages = [
-    # pkgs.kdePackages.kaccounts-integration
-    # pkgs.kdePackages.kaccounts-providers
-    # pkgs.kdePackages.kio-gdrive
-    # pkgs.kdePackages.kio-fuse
-    pkgs.plasma6Packages.kaccounts-integration
-    pkgs.plasma6Packages.kaccounts-providers
-    pkgs.plasma6Packages.kio-gdrive
-    pkgs.plasma6Packages.kio-fuse
+    pkgs.kdePackages.kaccounts-integration
+    pkgs.kdePackages.kaccounts-providers
+    pkgs.kdePackages.kio-gdrive
+    pkgs.kdePackages.kio-fuse
   ];
 
   # services.kio-fuse.enable = true;
