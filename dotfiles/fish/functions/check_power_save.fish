4312758764ec9eb63118cc91636dfaf220e09a00
Sa9m <abyssalflerken@gmail.com>
2025-11-27 13:07:52 -0600

    Update NixOS configuration on Thu Nov 27 01:07:52 PM CST 2025

--- dotfiles/fish/functions/check_power_save.fish
+++ dotfiles/fish/functions/check_power_save.fish
@@ -2,7 +2,7 @@ function check_power_save
     set current_profile (powerprofilesctl get)
 
     if test "$current_profile" = "power-saver"
-        echo '{ "text":"󱈏", "tooltip": "Mode: Eco", "class": "eco" }'
+        echo '{ "text":"󰌪", "tooltip": "Mode: Eco", "class": "eco" }'
     else if test "$current_profile" = "performance"
         echo '{ "text":"󰓅", "tooltip": "Mode: Performance", "class": "performance" }'
     else
