d26acd78be808e67985259d09a42962627cf66f3
Sa9m <abyssalflerken@gmail.com>
2025-11-27 12:28:50 -0600

    Update NixOS configuration on Thu Nov 27 12:28:50 PM CST 2025

--- dotfiles/fish/functions/check_power_save.fish
+++ dotfiles/fish/functions/check_power_save.fish
@@ -1,12 +1,12 @@
 function check_power_save
-    # Check if blur is currently enabled (1) or disabled (0)
-    set current_state (hyprctl getoption decoration:blur:enabled | awk '/int:/ {print $2}')
+    # Check the blur status. 'int: 1' means enabled (Balanced), 'int: 0' means disabled (Eco)
+    set status (hyprctl getoption decoration:blur:enabled | grep 'int: 1')
 
-    if test "$current_state" -eq 0
-        # Blur is OFF -> Power Save is ON
-        echo '{ "text":"󱈏", "tooltip": "power-save <span color=\'#a6da95\'>on</span>", "class": "on" }'
-    else
-        # Blur is ON -> Power Save is OFF
+    if test -n "$status"
+        # Blur is ENABLED (1) -> Power Save is OFF (Normal Mode)
         echo '{ "text":"󰁹", "tooltip": "power-save <span color=\'#ee99a0\'>off</span>", "class": "off" }'
+    else
+        # Blur is DISABLED (0) -> Power Save is ON (Eco Mode)
+        echo '{ "text":"󱈏", "tooltip": "power-save <span color=\'#a6da95\'>on</span>", "class": "on" }'
     end
 end
