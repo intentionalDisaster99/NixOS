78f6bf0ff032f9701bb1ba21b7e287ec2043da70
Sa9m <abyssalflerken@gmail.com>
2025-11-27 01:02:42 -0600

    Changed UUID for boot

--- hosts/gluon/hardware-configuration.nix
+++ hosts/gluon/hardware-configuration.nix
@@ -19,7 +19,7 @@
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/0413-77F7";
+    { device = "/dev/disk/by-uuid/1588-A8B5";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
