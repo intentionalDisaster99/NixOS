9ad630becd4c4d3e70dbfff618946dadf349da5d
Sa9m <abyssalflerken@gmail.com>
2025-11-27 12:42:45 -0600

    Update NixOS configuration on Thu Nov 27 12:42:45 PM CST 2025

--- dotfiles/fish/functions/check_power_save.fish
+++ dotfiles/fish/functions/check_power_save.fish
@@ -2,7 +2,7 @@ function check_power_save
     # Check the actual system power profile
     if powerprofilesctl get | grep -q "power-saver"
         # Power Saver is ACTIVE -> Eco Mode ON (Green Leaf)
-        echo '{ "text":"󱈏", "tooltip": "power-save <span color=\'#a6da95\'>on</span>", "class": "on" }'
+        echo '{ "text":"󱋙", "tooltip": "power-save <span color=\'#a6da95\'>on</span>", "class": "on" }'
     else
         # Balanced/Performance -> Eco Mode OFF (Battery Icon)
         echo '{ "text":"󰌪", "tooltip": "power-save <span color=\'#ee99a0\'>off</span>", "class": "off" }'
