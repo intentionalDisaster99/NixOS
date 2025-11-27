8b46f50df38dbf22e8eaf1788215fbee8a853474
Sa9m <abyssalflerken@gmail.com>
2025-11-27 11:25:41 -0600

    Update NixOS configuration on Thu Nov 27 11:25:41 AM CST 2025

--- hosts/gluon/hardware-configuration.nix
+++ hosts/gluon/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
@@ -14,12 +15,14 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/32bc026b-9d8f-4513-98b6-21fab85157ab";
+    {
+      device = "/dev/disk/by-uuid/32bc026b-9d8f-4513-98b6-21fab85157ab";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1588-A8B5";
+    {
+      device = "/dev/disk/by-uuid/1588-A8B5";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
