4d4d66a9ab1429474529a542b377dfa4e3359097
Sa9m <abyssalflerken@gmail.com>
2025-12-07 18:11:28 -0600

    Update NixOS configuration on Sun Dec  7 06:11:28 PM CST 2025

--- home.nix
+++ home.nix
@@ -43,55 +43,52 @@
   ################
   # Default Apps #
   ################
+  xdg.mimeApps =
+    let
+      my-associations = {
+        # --- Okular ---
+        "application/pdf" = "okularApplication_pdf.desktop";
+        "application/vnd.comicbook+zip" = "okularApplication_comicbook.desktop";
+        "application/vnd.comicbook-rar" = "okularApplication_comicbook.desktop";
+        "application/x-cbz" = "okularApplication_comicbook.desktop";
+        "application/x-cbr" = "okularApplication_comicbook.desktop";
+        "application/epub+zip" = "okularApplication_epub.desktop";
+        "application/x-mobipocket-ebook" = "okularApplication_mobi.desktop";
+        "application/x-fictionbook+xml" = "okularApplication_fb.desktop";
+        "image/vnd.djvu" = "okularApplication_djvu.desktop";
+        "image/tiff" = "okularApplication_tiff.desktop";
+        "application/x-dvi" = "okularApplication_dvi.desktop";
+        "application/vnd.ms-xpsdocument" = "okularApplication_xps.desktop";
+        "application/postscript" = "okularApplication_ghostview.desktop";
 
-  # WRAP IT IN THIS BLOCK:
-  xdg.mimeApps = {
-    enable = true;
+        # --- VSCode ---
+        "text/plain" = "code.desktop";
+        "text/markdown" = "code.desktop";
+        "application/x-shellscript" = "code.desktop";
+        "application/json" = "code.desktop";
+        "application/yaml" = "code.desktop";
+        "application/toml" = "code.desktop";
+        "text/x-cmake" = "code.desktop";
+        "text/x-python" = "code.desktop";
+        "text/x-go" = "code.desktop";
+        "text/rust" = "code.desktop";
+        "text/x-c++src" = "code.desktop";
+        "text/x-c" = "code.desktop";
+        "application/javascript" = "code.desktop";
+        "text/css" = "code.desktop";
+        "text/html" = "code.desktop";
 
-    defaultApplications = {
-      # --- Okular (Comics, PDFs, E-books, Images) ---
-      "application/pdf" = "okularApplication_pdf.desktop";
-
-      # Comic Books (cbz, cbr)
-      "application/vnd.comicbook+zip" = "okularApplication_comicbook.desktop";
-      "application/vnd.comicbook-rar" = "okularApplication_comicbook.desktop";
-      "application/x-cbz" = "okularApplication_comicbook.desktop";
-      "application/x-cbr" = "okularApplication_comicbook.desktop";
-
-      # E-books (epub, mobi, fb2)
-      "application/epub+zip" = "okularApplication_epub.desktop";
-      "application/x-mobipocket-ebook" = "okularApplication_mobi.desktop";
-      "application/x-fictionbook+xml" = "okularApplication_fb.desktop";
-
-      # Other Documents (DjVu, DVI, TIFF, XPS)
-      "image/vnd.djvu" = "okularApplication_djvu.desktop";
-      "image/tiff" = "okularApplication_tiff.desktop";
-      "application/x-dvi" = "okularApplication_dvi.desktop";
-      "application/vnd.ms-xpsdocument" = "okularApplication_xps.desktop";
-      "application/postscript" = "okularApplication_ghostview.desktop";
-
-      # --- VSCode (Text & Code) ---
-      "text/plain" = "code.desktop";
-      "text/markdown" = "code.desktop";
-      "application/x-shellscript" = "code.desktop";
-      "application/json" = "code.desktop";
-      "application/yaml" = "code.desktop";
-      "application/toml" = "code.desktop";
-      "text/x-cmake" = "code.desktop";
-      "text/x-python" = "code.desktop";
-      "text/x-go" = "code.desktop";
-      "text/rust" = "code.desktop";
-      "text/x-c++src" = "code.desktop";
-      "text/x-c" = "code.desktop";
-      "application/javascript" = "code.desktop";
-      "text/css" = "code.desktop";
-      "text/html" = "code.desktop";
-
-      # --- Web Browsing ---
-      "x-scheme-handler/http" = "brave-browser.desktop";
-      "x-scheme-handler/https" = "brave-browser.desktop";
-      "x-scheme-handler/about" = "brave-browser.desktop";
-      "x-scheme-handler/unknown" = "brave-browser.desktop";
+        # --- Web ---
+        "x-scheme-handler/http" = "brave-browser.desktop";
+        "x-scheme-handler/https" = "brave-browser.desktop";
+        "x-scheme-handler/about" = "brave-browser.desktop";
+        "x-scheme-handler/unknown" = "brave-browser.desktop";
+      };
+    in
+    {
+      enable = true;
+      # Apply the list to both places to satisfy Dolphin
+      defaultApplications = my-associations;
+      associations.added = my-associations;
     };
-  };
 }
