4f809766d6489d9bc4558b94cbc5673e3f0da227
Sa9m <abyssalflerken@gmail.com>
2026-01-07 12:46:39 -0600

    Update NixOS configuration on Wed Jan  7 12:46:39 PM CST 2026

--- dotfiles/fish/functions/autostart.fish
+++ dotfiles/fish/functions/autostart.fish
@@ -2,6 +2,6 @@ function autostart
     powerprofilesctl set balanced &
     hypridle & dunst & pypr & poweralertd -s & wl-paste --type text --watch cliphist store & wl-paste --type image --watch cliphist store & wl-clip-persist --clipboard regular & avizo-service & hyprpaper & systemctl --user start psi-notify
     sleep 3
-    handle_monitor_power
+    handle_monitor_power &
     waybar &
 end
