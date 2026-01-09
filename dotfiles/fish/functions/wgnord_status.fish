fc894f89623ed0cd7c273f89901a5ce4153171e6
Sa9m <abyssalflerken@gmail.com>
2026-01-09 17:15:15 -0600

    Update NixOS configuration on Fri Jan  9 05:15:15 PM CST 2026

--- dotfiles/fish/functions/wgnord_status.fish
+++ dotfiles/fish/functions/wgnord_status.fish
@@ -1,8 +1,8 @@
 function wgnord_status
     # Check if the interface 'wgnord' exists
     if ip link show wgnord > /dev/null 2>&1
-        echo '{"text": "\uf023", "tooltip": "VPN Connected", "class": "connected", "alt": "connected"}'
+        echo '{"text": "", "tooltip": "VPN Connected", "class": "connected", "alt": "connected"}'
     else
-        echo '{"text": "\uf09c", "tooltip": "VPN Disconnected", "class": "disconnected", "alt": "disconnected"}'
+        echo '{"text": "", "tooltip": "VPN Disconnected", "class": "disconnected", "alt": "disconnected"}'
     end
 end
