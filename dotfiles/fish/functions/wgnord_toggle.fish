9c222c19b744d1035a007a2578bbe0d1b4514988
Sa9m <abyssalflerken@gmail.com>
2026-01-09 16:07:25 -0600

    Update NixOS configuration on Fri Jan  9 04:07:25 PM CST 2026

--- dotfiles/fish/functions/wgnord_toggle.fish
+++ dotfiles/fish/functions/wgnord_toggle.fish
@@ -1,10 +1,12 @@
 function wgnord_toggle
     if systemctl is-active --quiet wg-quick-wgnord
-        notify-send "NordVPN" "Disconnecting..."
+        notify-send "NordVPN" "Disconnecting"
         # This will trigger your GUI password prompt (Polkit)
-        systemctl stop wg-quick-wgnord
+        # systemctl stop wg-quick-wgnord
+        wgnord d
     else
-        notify-send "NordVPN" "Connecting..."
-        systemctl start wg-quick-wgnord
+        notify-send "NordVPN" "Connecting to United States"
+        # systemctl start wg-quick-wgnord
+        wgnord c "United States"
     end
 end
