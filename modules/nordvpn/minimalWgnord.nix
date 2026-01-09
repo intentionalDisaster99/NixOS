1613e18cea94f557b712ac34cc92949f4428fc0e
Sa9m <abyssalflerken@gmail.com>
2026-01-09 15:32:56 -0600

    Update NixOS configuration on Fri Jan  9 03:32:56 PM CST 2026

--- modules/nordvpn/minimalWgnord.nix
+++ modules/nordvpn/minimalWgnord.nix
@@ -3,7 +3,6 @@
 let
   cfg = config.services.wgnord;
 
-  # We explicitly include wireguard-tools so wg-quick is available
   wgnord = pkgs.wgnord;
   wgTools = pkgs.wireguard-tools;
 
@@ -36,7 +35,7 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    # Ensure system has the tools needed for manual debugging too
+    # Install tools globally so you can use them manually too
     environment.systemPackages = [ wgnord wgTools pkgs.iproute2 pkgs.openresolv ];
 
     systemd.services.wgnord = {
@@ -44,24 +43,27 @@ in
       after = [ "network-online.target" ];
       wants = [ "network-online.target" ];
 
-      # Add necessary tools to the service PATH
-      path = [ wgnord wgTools pkgs.iproute2 pkgs.iptables ];
+      # FIX: Add 'openresolv' and 'systemd' so wg-quick can set DNS
+      path = [
+        wgnord
+        wgTools
+        pkgs.iproute2
+        pkgs.iptables
+        pkgs.openresolv
+        pkgs.systemd
+      ];
 
       serviceConfig = {
         Type = "oneshot";
         StateDirectory = "wgnord";
         StateDirectoryMode = "0700";
 
-        # SCRIPT: Create dirs, link template, login
         ExecStartPre = pkgs.writeShellScript "wgnord-pre" ''
           set -e
           mkdir -p /var/lib/wgnord
           mkdir -p /etc/wireguard
           chmod 700 /var/lib/wgnord /etc/wireguard
-          
           ln -fs ${template} /var/lib/wgnord/template.conf
-          
-          # Login (using the file content)
           ${lib.getExe wgnord} login "$(<${cfg.tokenFile})"
         '';
 
