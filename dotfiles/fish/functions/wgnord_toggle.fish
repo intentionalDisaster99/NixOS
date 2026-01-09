079e0b652c3d97111ea2fb422318ecbf0562ec3f
Sa9m <abyssalflerken@gmail.com>
2026-01-09 16:55:58 -0600

    Update NixOS configuration on Fri Jan  9 04:55:58 PM CST 2026

--- dotfiles/fish/functions/wgnord_toggle.fish
+++ dotfiles/fish/functions/wgnord_toggle.fish
@@ -1,12 +1,9 @@
 function wgnord_toggle
-    # Check if the interface exists
     if ip link show wgnord > /dev/null 2>&1
         notify-send "NordVPN" "Disconnecting..."
-        # Stop the correct service name
         sudo systemctl stop wgnord
     else
         notify-send "NordVPN" "Connecting..."
-        # Start the correct service name
         sudo systemctl start wgnord
     end
 end
