c6d09f9c76944f230e4dc46f658bc44dc444ab46
Sa9m <abyssalflerken@gmail.com>
2026-01-09 19:40:29 -0600

    Update NixOS configuration on Fri Jan  9 07:40:29 PM CST 2026

--- dotfiles/fish/functions/tailscale_status.fish
+++ dotfiles/fish/functions/tailscale_status.fish
@@ -5,9 +5,9 @@ function tailscale_status
     # -W 1: Wait max 1 second for a reply
     if ping -c 1 -W 1 100.100.100.100 > /dev/null 2>&1
         # Success: We can reach the internal mesh
-        printf '{"text": "", "tooltip": "Tailnet Reachable", "class": "connected", "alt": "connected"}\n'
+        printf '{"text": "󰒍", "tooltip": "Tailnet Reachable", "class": "connected", "alt": "connected"}\n'
     else
         # Fail: Cannot reach the mesh (Blocked or Stopped)
-        printf '{"text": "󰌙", "tooltip": "Tailnet Unreachable", "class": "disconnected", "alt": "disconnected"}\n'
+        printf '{"text": "󰒎", "tooltip": "Tailnet Unreachable", "class": "disconnected", "alt": "disconnected"}\n'
     end
 end
