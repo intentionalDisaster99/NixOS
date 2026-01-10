94fab929f6ab30d837ec7c72e521273ee2e5141a
Sa9m <abyssalflerken@gmail.com>
2026-01-09 20:33:25 -0600

    Update NixOS configuration on Fri Jan  9 08:33:25 PM CST 2026

--- dotfiles/fish/functions/check_power_save.fish
+++
@@ -1,9 +0,0 @@
-function check_power_save
-    if test -f /tmp/eco_mode
-        # JSON for "Enabled" 
-        echo '{"text": "󰿎", "tooltip": "Moving Wallpaper On", "class": "active", "alt": "active"}'
-    else
-        # JSON for "Disabled" 
-        echo '{"text": "", "tooltip": "Photo Wallpaper Enabled", "class": "inactive", "alt": "inactive"}'
-    end
-end
