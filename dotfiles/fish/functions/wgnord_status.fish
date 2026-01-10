77007dcc3d312f47e8eb70711ad6e5db2763360b
Sa9m <abyssalflerken@gmail.com>
2026-01-09 19:15:16 -0600

    Update NixOS configuration on Fri Jan  9 07:15:16 PM CST 2026

--- dotfiles/fish/functions/wgnord_status.fish
+++ dotfiles/fish/functions/wgnord_status.fish
@@ -1,9 +1,7 @@
 function wgnord_status
     if ip link show wgnord > /dev/null 2>&1
-        # Connected: Use the 'Locked' icon from your keyboard module (󰌾)
         printf '{"text": "󰌾", "tooltip": "VPN Connected", "class": "connected", "alt": "connected"}\n'
     else
-        # Disconnected: Use the 'Unlocked' icon from your keyboard module (󰍀)
         printf '{"text": "󰍀", "tooltip": "VPN Disconnected", "class": "disconnected", "alt": "disconnected"}\n'
     end
 end
