241aeb50b65522fe76e586cc4f945dcd32e2e00c
Sa9m <abyssalflerken@gmail.com>
2026-01-09 20:29:00 -0600

    Update NixOS configuration on Fri Jan  9 08:29:00 PM CST 2026

---
+++ dotfiles/fish/functions/wall_toggle.fish
@@ -0,0 +1,22 @@
+function wall_toggle
+    set MODE_FILE "/tmp/wallpaper_mode"
+
+    if not test -f $MODE_FILE
+        echo "normal" > $MODE_FILE
+    end
+
+    set CURRENT (cat $MODE_FILE)
+
+    if test "$CURRENT" = "normal"
+        echo "forced" > $MODE_FILE
+        notify-send -u low -t 2000 "Wallpaper: FORCED ON" "Ignoring Battery & CPU"
+    else if test "$CURRENT" = "forced"
+        echo "off" > $MODE_FILE
+        notify-send -u low -t 2000 "Wallpaper: OFF" "Saving Power"
+    else
+        echo "normal" > $MODE_FILE
+        notify-send -u low -t 2000 "Wallpaper: NORMAL" "Smart Power/CPU Saving"
+    end
+    
+    
+end
