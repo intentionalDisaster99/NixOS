1cffacd724b4e720a144e252db7abfdb7b49f956
Sa9m <abyssalflerken@gmail.com>
2026-01-09 20:05:58 -0600

    Update NixOS configuration on Fri Jan  9 08:05:58 PM CST 2026

--- dotfiles/fish/functions/check_power_save.fish
+++ dotfiles/fish/functions/check_power_save.fish
@@ -1,9 +1,9 @@
 function check_power_save
     if test -f /tmp/eco_mode
         # JSON for "Enabled" 
-        echo '{"text": "", "tooltip": "Eco Mode: ON", "class": "active", "alt": "active"}'
+        echo '{"text": "", "tooltip": "Eco Mode: ON", "class": "active", "alt": "active"}'
     else
         # JSON for "Disabled" 
-        echo '{"text": "", "tooltip": "Eco Mode: OFF", "class": "inactive", "alt": "inactive"}'
+        echo '{"text": "󰎁", "tooltip": "Eco Mode: OFF", "class": "inactive", "alt": "inactive"}'
     end
 end
