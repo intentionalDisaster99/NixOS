2584c046a45f9fd5cc6452ce493873bcbc6226fa
Sa9m <abyssalflerken@gmail.com>
2026-01-09 23:11:41 -0600

    Update NixOS configuration on Fri Jan  9 11:11:41 PM CST 2026

--- dotfiles/fish/functions/handle_wallpaper.fish
+++ dotfiles/fish/functions/handle_wallpaper.fish
@@ -2,15 +2,11 @@ function handle_wallpaper
     set PID_FILE "/tmp/mpvpaper.pid"
     set MODE_FILE "/tmp/wallpaper_mode"
     
-    # Configuration
     set VIDEO_WALLPAPER "/etc/nixos/resources/wallpapers/car.mp4" 
     set MONITOR "*" 
     set CPU_LIMIT 80
 
-    # Ensure we start in Normal mode if no file exists
-    if not test -f $MODE_FILE
-        echo "normal" > $MODE_FILE
-    end
+    echo "normal" > $MODE_FILE
 
     # Clean start
     killall -q mpvpaper
@@ -25,14 +21,12 @@ function handle_wallpaper
     set PREV_IDLE $vals[4]
 
     while true
-        # 1. READ CURRENT MODE
         if test -f $MODE_FILE
             set MODE (cat $MODE_FILE)
         else
             set MODE "normal"
         end
 
-        # 2. DETERMINE DESIRED STATE (Should it be running?)
         set SHOULD_RUN 0
 
         if test "$MODE" = "off"
@@ -40,9 +34,7 @@ function handle_wallpaper
         else if test "$MODE" = "forced"
             set SHOULD_RUN 1
         else
-            # --- NORMAL MODE LOGIC (Smart) ---
             
-            # A. Calculate CPU
             set cpu_line (grep '^cpu ' /proc/stat)
             set cpu_str (string trim (string replace -r '^cpu\s+' '' (string replace -r -a '\s+' ' ' $cpu_line)))
             set vals (string split " " $cpu_str)
@@ -59,14 +51,12 @@ function handle_wallpaper
             set PREV_TOTAL $CURRENT_TOTAL
             set PREV_IDLE $CURRENT_IDLE
 
-            # B. Check Power (Default to 1 if sensor fails)
             if test -e /sys/class/power_supply/ADP1/online
                 set ADAPTER (cat /sys/class/power_supply/ADP1/online)
             else
                 set ADAPTER 1
             end
 
-            # C. Decision
             if test "$ADAPTER" = "1"; and test $CPU_USAGE -lt $CPU_LIMIT
                 set SHOULD_RUN 1
             else
@@ -74,8 +64,6 @@ function handle_wallpaper
             end
         end
 
-        # 3. APPLY STATE
-        # Check if it is currently running
         set IS_RUNNING 0
         if test -f $PID_FILE
             set CURRENT_PID (cat $PID_FILE)
@@ -88,8 +76,6 @@ function handle_wallpaper
 
         if test "$SHOULD_RUN" = "1"
             if test "$IS_RUNNING" = "0"
-                # START IT
-                # Using nohup and disown to ensure it survives correctly
                 nohup mpvpaper -o "no-audio --loop-file=inf --profile=fast --hwdec=no" $MONITOR $VIDEO_WALLPAPER >/dev/null 2>&1 &
                 set NEW_PID $last_pid
                 echo $NEW_PID > $PID_FILE
