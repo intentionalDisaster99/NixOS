2a4f8551dc7892499e6fe0f56698a5657e3d1c98
Sa9m <abyssalflerken@gmail.com>
2026-01-10 14:36:27 -0600

    Update NixOS configuration on Sat Jan 10 02:36:27 PM CST 2026

--- modules/programs/programs.nix
+++ modules/programs/programs.nix
@@ -127,4 +127,22 @@
     tokenFile = "/home/sa9m/.confidential/wgnordToken.txt";
   };
 
+
+  # Mounting my NAS TODO Move to a module
+  {
+  # For mount.cifs, required unless domain name resolution is not needed.
+  environment.systemPackages = [ pkgs.cifs-utils ];
+  fileSystems."/mnt/NAS" = {
+    device = "//graviton/Share";
+    fsType = "cifs";
+    options =
+      let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+  };
+}
+
 }
