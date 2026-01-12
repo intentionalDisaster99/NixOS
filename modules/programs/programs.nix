ccd59c4dd7975af85233ec1916833586e5a66c09
Sa9m <abyssalflerken@gmail.com>
2026-01-12 10:33:44 -0600

    Update NixOS configuration on Mon Jan 12 10:33:44 AM CST 2026

--- modules/programs/programs.nix
+++ modules/programs/programs.nix
@@ -145,4 +145,14 @@
 
   };
 
+  programs.atuin = {
+    enable = true;
+    settings = {
+      # auto_sync = true;
+      # sync_frequency = "5m";
+      # sync_address = "https://api.atuin.sh";
+      search_mode = "fuzzy";
+    };
+  };
+
 }
