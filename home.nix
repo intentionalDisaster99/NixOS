77026bfc2946467d3471ea343017d14223742109
Sa9m <abyssalflerken@gmail.com>
2026-01-12 10:35:03 -0600

    Update NixOS configuration on Mon Jan 12 10:35:03 AM CST 2026

--- home.nix
+++ home.nix
@@ -91,4 +91,15 @@
       defaultApplications = associations;
       associations.added = associations;
     };
+
+  # TODO see if I can move this to a module
+  programs.atuin = {
+    enable = true;
+    settings = {
+      # auto_sync = true;
+      # sync_frequency = "5m";
+      # sync_address = "https://api.atuin.sh";
+      search_mode = "fuzzy";
+    };
+  };
 }
