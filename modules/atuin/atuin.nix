4239a2e00116fd5d174823707bc2e8ac7a3ad978
Sa9m <abyssalflerken@gmail.com>
2026-01-12 10:33:44 -0600

    Update NixOS configuration on Mon Jan 12 10:33:44 AM CST 2026

--- modules/atuin/atuin.nix
+++ modules/atuin/atuin.nix
@@ -1,12 +1,4 @@
 # A better terminal history
 { pkgs, ... }: {
-  programs.atuin = {
-    enable = true;
-    settings = {
-      # auto_sync = true;
-      # sync_frequency = "5m";
-      # sync_address = "https://api.atuin.sh";
-      search_mode = "fuzzy";
-    };
-  };
+  # TODO move to here
 }
