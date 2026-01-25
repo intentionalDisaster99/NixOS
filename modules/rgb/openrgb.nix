bf8820cf01b2442533fdb9b115fbafe4bd618ff8
Sam Whitlock <abyssalflerken@gmail.com>
2026-01-25 12:46:27 -0600

    Update NixOS configuration on Sun Jan 25 12:46:27 PM CST 2026

--- modules/rgb/openrgb.nix
+++ modules/rgb/openrgb.nix
@@ -4,8 +4,18 @@
 
 {
 
-  environment.systemPackages = with pkgs; [ openrgb-with-all-plugins ];
+  # environment.systemPackages = with pkgs; [ openrgb-with-all-plugins ];
 
-  services.hardware.openrgb.enable = true;
+  # services.hardware.openrgb.enable = true;
+  services.hardware.openrgb = {
+    enable = true;
+    package = pkgs.openrgb-with-all-plugins;
+    motherboard = "amd";
+    server = {
+      port = 6742;
+      autoStart = true;
+    };
+  };
+
 
 }
