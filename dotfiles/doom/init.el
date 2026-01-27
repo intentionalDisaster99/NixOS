315ef3ae903aec6d9a6c4d5268dd6aab5b20cdc8
Sam Whitlock <abyssalflerken@gmail.com>
2026-01-26 23:48:57 -0600

    Emacs updates

--- dotfiles/doom/init.el
+++ dotfiles/doom/init.el
@@ -146,6 +146,7 @@
        json              ; At least it ain't XML
        ;;janet             ; Fun fact: Janet is me!
        (java +lsp)       ; the poster child for carpal tunnel syndrome
+       (lsp)             ; For getting javadocs to work apparently
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
