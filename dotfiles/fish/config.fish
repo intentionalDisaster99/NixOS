66397547370fa61e64fb9fea5782ed96273f81ee
Sa9m <abyssalflerken@gmail.com>
2026-02-02 21:03:40 -0600

    Update NixOS configuration on Mon Feb  2 09:03:40 PM CST 2026 for higgs-boson

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    if set -q IN_NIX_SHELL
+        set_color -o green
+        echo "❄️ nix-shell "
+        set_color normal
+    end
 end
 
 alias cl="clear"
