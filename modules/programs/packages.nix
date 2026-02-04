041f727191e1b4d1b6ef1d9c8617fa0c040a9b3c
Sa9m <abyssalflerken@gmail.com>
2026-02-04 16:29:59 -0600

    Update NixOS configuration on Wed Feb  4 04:29:59 PM CST 2026 for higgs-boson

--- modules/programs/packages.nix
+++ modules/programs/packages.nix
@@ -189,7 +189,7 @@
     # TMP
     rpi-imager
 
-    (builtins.getFlake "github:astrada/google-drive-ocamlfuse").packages.x86_64-linux.default
+    # (builtins.getFlake "github:astrada/google-drive-ocamlfuse").packages.x86_64-linux.default
 
   ];
 
