316be02dbf8f9f0a7e2f31b069a22e54c9681226
Sa9m <abyssalflerken@gmail.com>
2026-02-04 14:46:51 -0600

    Update NixOS configuration on Wed Feb  4 02:46:51 PM CST 2026 for higgs-boson

--- modules/google-drive/google-drive.nix
+++ modules/google-drive/google-drive.nix
@@ -1,22 +1,23 @@
-{ config, pkgs, lib, inputs, ... }:
-
 {
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "opam-nix/nixpkgs";
+  };
+  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
+    let package = "google-drive-ocamlfuse";
+    in flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        scope =
+          on.buildOpamProject { } package ./. { ocaml-base-compiler = "*"; };
+        overlay = final: prev:
+          { };
+      in
+      {
+        legacyPackages = scope.overrideScope overlay;
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  environment.systemPackages = with pkgs; [
-    (builtins.getFlake "github:astrada/google-drive-ocamlfuse").packages.x86_64-linux.default
-  ];
-
-  # services.hardware.openrgb.enable = true;
-  #   services.hardware.openrgb = {
-  #     enable = true;
-  #     package = pkgs.openrgb-with-all-plugins;
-  #     motherboard = "amd";
-  #   };
-
-
+        packages.default = self.legacyPackages.${system}.${package};
+      });
 }
