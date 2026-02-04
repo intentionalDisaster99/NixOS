31008335f271bc1caa3d419daef6584c5a985f57
Sa9m <abyssalflerken@gmail.com>
2026-02-04 14:43:47 -0600

    Update NixOS configuration on Wed Feb  4 02:43:47 PM CST 2026 for higgs-boson

--- modules/google-drive/google-drive.nix
+++ modules/google-drive/google-drive.nix
@@ -1,17 +1,26 @@
-{ config, pkgs, lib, inputs, ... }:
+# { config, pkgs, lib, inputs, ... }:
+
+# {
+
+#   environment.systemPackages = with pkgs; [
+#     (builtins.getFlake "github:astrada/google-drive-ocamlfuse").packages.x86_64-linux.default
+#   ];
+
+#   # services.hardware.openrgb.enable = true;
+#   #   services.hardware.openrgb = {
+#   #     enable = true;
+#   #     package = pkgs.openrgb-with-all-plugins;
+#   #     motherboard = "amd";
+#   #   };
+
+
+# }
 
 {
-
   environment.systemPackages = with pkgs; [
+
+    # ...
+
     (builtins.getFlake "github:astrada/google-drive-ocamlfuse").packages.x86_64-linux.default
   ];
-
-  # services.hardware.openrgb.enable = true;
-  #   services.hardware.openrgb = {
-  #     enable = true;
-  #     package = pkgs.openrgb-with-all-plugins;
-  #     motherboard = "amd";
-  #   };
-
-
 }
