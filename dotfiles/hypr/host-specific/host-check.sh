c69def8c236926315a2cdf9a12f80dbdc6e426ca
Sam Whitlock <abyssalflerken@gmail.com>
2026-02-07 23:38:45 -0600

    Update NixOS configuration on Sat Feb  7 11:38:45 PM CST 2026 for gluon

--- dotfiles/hypr/host-specific/host-check.sh
+++ dotfiles/hypr/host-specific/host-check.sh
@@ -2,10 +2,10 @@
 
 case "$(hostname)" in
   gluon)
-    echo "source = $HOME/dotfiles/hypr/host-specific/gluon.conf" > /tmp/hypr-host.conf
+    echo "source = /etc/nixos/dotfiles/hypr/host-specific/gluon.conf" > /tmp/hypr-host.conf
     ;;
   higgs-boson)
-    echo "source = $HOME/dotfiles/hypr/host-specific/higgs-boson.conf" > /tmp/hypr-host.conf
+    echo "source = /etc/nixos/dotfiles/hypr/host-specific/higgs-boson.conf" > /tmp/hypr-host.conf
     ;;
   *)
     echo "# no host-specific config" > /tmp/hypr-host.conf
