5c9ea895b053de1414a45a332f7a390c1bc251a5
Sa9m <abyssalflerken@gmail.com>
2026-02-11 20:41:15 -0600

    Update NixOS configuration on Wed Feb 11 08:41:15 PM CST 2026 for higgs-boson

--- modules/google-drive/google-drive.nix
+++ modules/google-drive/google-drive.nix
@@ -1,17 +1,37 @@
 { pkgs, ... }:
 
+let
+  # Import the secrets from 2 folders up
+  driveSecrets = import ../../secrets/google-drive.nix;
+in
 {
   environment.systemPackages = [ pkgs.google-drive-ocamlfuse pkgs.fuse ];
 
   systemd.user.services.google-drive-mount = {
     description = "Google Drive Mount";
     wantedBy = [ "default.target" ];
+
     serviceConfig = {
-      ExecStart = "${pkgs.google-drive-ocamlfuse}/bin/google-drive-ocamlfuse %h/GDrive/MyDrive";
-      ExecStop = "${pkgs.fuse}/bin/fusermount -u %h/GDrive/MyDrive";
+      # This is where we inject the credentials
+      ExecStart = ''
+        ${pkgs.google-drive-ocamlfuse}/bin/google-drive-ocamlfuse \
+          -id "${driveSecrets.client_id}" \
+          -secret "${driveSecrets.client_secret}" \
+          %h/GoogleDrive
+      '';
+
+      ExecStop = "${pkgs.fuse}/bin/fusermount -u %h/GoogleDrive";
       Restart = "on-failure";
       Type = "forking";
     };
-    preStart = "${pkgs.coreutils}/bin/mkdir -p %h/GDrive/MyDrive";
+
+    # Ensure the directory exists
+    preStart = "${pkgs.coreutils}/bin/mkdir -p %h/GoogleDrive";
+
+    # Create the symlink for Shared Drives
+    postStart = ''
+      ${pkgs.coreutils}/bin/sleep 2
+      ${pkgs.coreutils}/bin/ln -sfn %h/GoogleDrive/.shared %h/SharedDrives
+    '';
   };
 }
