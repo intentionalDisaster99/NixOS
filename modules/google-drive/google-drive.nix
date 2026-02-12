cbb2e1a6256634dc886f622159e4e321d90a3aad
Sa9m <abyssalflerken@gmail.com>
2026-02-11 20:34:54 -0600

    Update NixOS configuration on Wed Feb 11 08:34:54 PM CST 2026 for higgs-boson

--- modules/google-drive/google-drive.nix
+++ modules/google-drive/google-drive.nix
@@ -1,33 +1,33 @@
 { pkgs, ... }:
-
+THIS_SHOULD_FAIL_THE_BUILD
 let
-  driveSecrets = import "${./../../secrets/google-drive.nix}";
+driveSecrets = import "${./../../secrets/google-drive.nix}";
 in
 {
-  environment.systemPackages = [ pkgs.google-drive-ocamlfuse pkgs.fuse ];
+environment.systemPackages = [ pkgs.google-drive-ocamlfuse pkgs.fuse ];
 
-  systemd.user.services.google-drive-mount = {
-    description = "Google Drive Mount";
-    wantedBy = [ "default.target" ];
+systemd.user.services.google-drive-mount = {
+description = "Google Drive Mount";
+wantedBy = [ "default.target" ];
 
-    serviceConfig = {
-      ExecStart = ''
+serviceConfig = {
+ExecStart = ''
         ${pkgs.google-drive-ocamlfuse}/bin/google-drive-ocamlfuse \
           -id "${driveSecrets.client_id}" \
           -secret "${driveSecrets.client_secret}" \
           %h/GoogleDrive
       '';
 
-      ExecStop = "${pkgs.fuse}/bin/fusermount -u %h/GoogleDrive";
-      Restart = "on-failure";
-      Type = "forking";
-    };
+ExecStop = "${pkgs.fuse}/bin/fusermount -u %h/GoogleDrive";
+Restart = "on-failure";
+Type = "forking";
+};
 
-    preStart = "${pkgs.coreutils}/bin/mkdir -p %h/GoogleDrive";
+preStart = "${pkgs.coreutils}/bin/mkdir -p %h/GoogleDrive";
 
-    postStart = ''
+postStart = ''
       ${pkgs.coreutils}/bin/sleep 2
       ${pkgs.coreutils}/bin/ln -sfn %h/GoogleDrive/.shared %h/SharedDrives
     '';
-  };
+};
 }
