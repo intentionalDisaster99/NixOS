192b0cb0eb593793a7a2ee2780d919fa0b6700d9
Sa9m <abyssalflerken@gmail.com>
2026-02-25 14:07:15 -0600

    Update NixOS configuration on Wed Feb 25 02:07:15 PM CST 2026 for higgs-boson

---
+++ modules/keyboard/steno.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.programs.steno;
+in
+{
+  options.programs.steno = {
+    enable = mkEnableOption "stenography support via Plover";
+  };
+
+  config = mkIf cfg.enable {
+    # Install the Plover package
+    environment.systemPackages = with pkgs; [
+      plover.dev # The dev branch often has better Wayland support
+    ];
+
+    # Enable uinput hardware module
+    hardware.uinput.enable = true;
+
+    # Set up udev rules to allow Plover to create a virtual keyboard
+    services.udev.extraRules = ''
+      # Allow access to uinput so Plover can inject translated keystrokes
+      KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+    '';
+
+    # Add your user to the necessary groups for steno to work
+    users.users.sa9m.extraGroups = [
+      "input"
+      "dialout"
+      "uinput"
+    ];
+
+    # Turning it on - hol up...
+    programs.steno.enable = true;
+  };
+}
