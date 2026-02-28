00932b5f994c145db6db7557fe3e74a82c94c255
Sa9m <abyssalflerken@gmail.com>
2026-02-27 20:35:46 -0600

    Update NixOS configuration on Fri Feb 27 08:35:46 PM CST 2026 for higgs-boson

--- dotfiles/fish/functions/power_save_toggle.fish
+++ dotfiles/fish/functions/power_save_toggle.fish
@@ -1,11 +1,35 @@
 function power_save_toggle
-    if test -f /tmp/eco_mode
-        rm /tmp/eco_mode
-        notify-send -u low "Eco Mode: OFF" "Video wallpaper enabled (if plugged in)"
-    else
-        touch /tmp/eco_mode
-        notify-send -u low "Eco Mode: ON" "Video wallpaper disabled"
+    
+    set MODE_FILE "/tmp/battery_saver_mode"
+
+    # Defaulting to inactive
+    if not test -f $MODE_FILE
+        echo "normal" > $MODE_FILE
     end
-    # Refresh the waybar module immediately
+
+    set CURRENT (cat $MODE_FILE)
+
+    if test "$CURRENT" = "battery_saver"
+
+        # Saving the current mode
+        echo "normal" > $MODE_FILE
+
+        # Turning off the wallpaper
+        echo "off" > "/tmp/wallpaper_mode"
+
+        notify-send -u low -t 2000 "Battery saver turned off."
+    else if test "$CURRENT" = "normal"
+
+        # Saving the mode 
+        echo "battery_saver" > $MODE_FILE
+
+        # Turning on the wallpaper to normal mode
+        echo "normal" > "/tmp/wallpaper_mode"
+
+        notify-send -u low -t 2000 "Battery saver turned on."
+    end
+
+    # Changing the waybar to use the alt format 
     pkill -SIGRTMIN+8 waybar
+    
 end
