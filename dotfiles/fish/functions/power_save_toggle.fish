c32d02d95d8eb5195a028bc8c1f0c1bef4305513
Sa9m <abyssalflerken@gmail.com>
2026-02-27 21:00:23 -0600

    Update NixOS configuration on Fri Feb 27 09:00:23 PM CST 2026 for higgs-boson

--- dotfiles/fish/functions/power_save_toggle.fish
+++ dotfiles/fish/functions/power_save_toggle.fish
@@ -1,6 +1,7 @@
 function power_save_toggle
     
     set MODE_FILE "/tmp/battery_saver_mode"
+    set CONFIG_DIR="dotfiles/waybar"
 
     # Defaulting to inactive
     if not test -f $MODE_FILE
@@ -14,6 +15,9 @@ function power_save_toggle
         # Saving the current mode
         echo "normal" > $MODE_FILE
 
+        # Switching to the normal waybar
+        ln -sf config-normal "$CONFIG_DIR/config"
+
         # Turning off the wallpaper
         echo "off" > "/tmp/wallpaper_mode"
 
@@ -23,13 +27,16 @@ function power_save_toggle
         # Saving the mode 
         echo "battery_saver" > $MODE_FILE
 
+        # Switching to the low power config
+        ln -sf config-lowpower "$CONFIG_DIR/config"
+
         # Turning on the wallpaper to normal mode
         echo "normal" > "/tmp/wallpaper_mode"
 
         notify-send -u low -t 2000 "Battery saver turned on."
     end
 
-    # Changing the waybar to use the alt format 
-    pkill -SIGRTMIN+8 waybar
-    
+    # Send reload signal to Waybar
+    killall -SIGUSR2 waybar
+
 end
