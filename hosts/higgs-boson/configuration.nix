d0d97f474f5236273cc36309b7e1daffe0a71567
Sa9m <abyssalflerken@gmail.com>
2026-03-02 11:35:00 -0600

    Update NixOS configuration on Mon Mar  2 11:35:00 AM CST 2026 for higgs-boson

--- hosts/higgs-boson/configuration.nix
+++ hosts/higgs-boson/configuration.nix
@@ -83,7 +83,7 @@
   };
 
   # Upgrading to the most recent kernel 
-  boot.kernelPackages = pkgs.linuxPackages_testin;
+  boot.kernelPackages = pkgs.linuxPackages_testing;
 
   # Adding in extra swap so that hibernation works
   swapDevices = [
