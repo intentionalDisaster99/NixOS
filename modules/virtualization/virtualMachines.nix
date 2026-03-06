26730532818d5d056de9861dcabae37ab8654353
Sa9m <abyssalflerken@gmail.com>
2026-03-06 13:43:51 -0600

    Update NixOS configuration on Fri Mar  6 01:43:51 PM CST 2026 for higgs-boson

--- modules/virtualization/virtualMachines.nix
+++ modules/virtualization/virtualMachines.nix
@@ -1,21 +1,29 @@
-# Currently does not work. I'll look into that soon, don't worry
-
 { config, pkgs, lib, ... }:
 
 {
-
+  # Enable virt-manager
   programs.virt-manager.enable = true;
-  users.groups.libvirtd.members = [ "sa9m" ];
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
-  # virtualisation.libvirtd.qemu = {
-  #   package = pkgs.qemu_kvm;
-  #   runAsRoot = true;
-  #   swtpm.enable = true;
-  #   ovmf = {
-  #     enable = true;
-  #     package = (pkgs.OVMFFull.override { secureBoot = true; tpmSupport = true; });
-  #   };
-  # };
 
+  # Add your user to the libvirtd group 
+  # (Using users.users.<name>.extraGroups is the standard NixOS convention)
+  users.users.sa9m.extraGroups = [ "libvirtd" ];
+
+  virtualisation = {
+    # Enable USB redirection for Spice
+    spiceUSBRedirection.enable = true;
+
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          # 'packages' expects a list. OVMFFull.fd contains both TPM and Secure Boot support natively.
+          packages = [ pkgs.OVMFFull.fd ];
+        };
+      };
+    };
+  };
 }
