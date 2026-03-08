0253a0c715e2d26aa550fb096a76c6224160ad3c
Sa9m <abyssalflerken@gmail.com>
2026-03-07 20:55:35 -0600

    Update NixOS configuration on Sat Mar  7 08:55:35 PM CST 2026 for higgs-boson

--- hosts/higgs-boson/configuration.nix
+++ hosts/higgs-boson/configuration.nix
@@ -117,19 +117,6 @@
     SUBSYSTEM=="usb", ATTRS{idVendor}=="c251", ATTRS{idProduct}=="f000", MODE="0666"
   '';
 
-  # Bootloader extra entries
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.extraEntries = ''
-    menuentry "Windows 11" {
-      insmod part_gpt
-      insmod fat
-      insmod search_fs_uuid
-      insmod chain
-      search --fs-uuid --set=root 7282-E320
-      chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-    }
-  '';
-
   # Adding in fonts for my code editors
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
