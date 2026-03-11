67cafd5eaaec8d1bba1dad33a3911c0fdfcd5ce8
Sa9m <abyssalflerken@gmail.com>
2026-03-11 00:29:26 -0500

    Update NixOS configuration on Wed Mar 11 12:29:26 AM CDT 2026 for higgs-boson

--- modules/random-cool-stuff/alien.nix
+++ modules/random-cool-stuff/alien.nix
@@ -2,7 +2,7 @@
 
 {
   environment.systemPackages = [
-    inputs.nix-alien.packages.${stdenv.hostPlatform.system}.nix-alien
+    inputs.nix-alien.packages.${pkgs.stdenv.hostPlatform.system}.nix-alien
   ];
 
   programs.nix-ld.enable = true;
