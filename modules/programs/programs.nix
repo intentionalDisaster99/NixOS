d8ffa284b74eb452bc4569e032243b1f08011073
Sa9m <abyssalflerken@gmail.com>
2026-03-12 20:30:26 -0500

    Update NixOS configuration on Thu Mar 12 08:30:26 PM CDT 2026 for higgs-boson

--- modules/programs/programs.nix
+++ modules/programs/programs.nix
@@ -130,19 +130,19 @@
   # Mounting my NAS TODO Move to a module
 
   # For mount.cifs, required unless domain name resolution is not needed.
-  environment.systemPackages = [ pkgs.cifs-utils ];
-  fileSystems."/mnt/NAS" = {
-    device = "//graviton/Share";
-    fsType = "cifs";
-    options =
-      let
-        # this line prevents hanging on network split
-        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+  # environment.systemPackages = [ pkgs.cifs-utils ];
+  # fileSystems."/mnt/NAS" = {
+  #   device = "//graviton/Share";
+  #   fsType = "cifs";
+  #   options =
+  #     let
+  #       # this line prevents hanging on network split
+  #       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
-      in
-      [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+  #     in
+  #     [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
 
-  };
+  # };
 
 
   # TODO create a pay-respects module
