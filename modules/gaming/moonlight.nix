2e9911a590589c497f6a9172d2fcf4d36fa40c84
Sa9m <abyssalflerken@gmail.com>
2026-03-16 13:54:06 -0500

    Update NixOS configuration on Mon Mar 16 01:54:06 PM CDT 2026 for higgs-boson

--- modules/gaming/moonlight.nix
+++ modules/gaming/moonlight.nix
@@ -3,4 +3,14 @@
   environment.systemPackages = with pkgs; [
     moonlight-qt
   ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver
+      libvdpau-va-gl
+    ];
+  };
 }
