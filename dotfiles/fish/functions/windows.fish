30e5e8e4fc3da6e13d4891e51460bc62024c15da
Sam Whitlock <abyssalflerken@gmail.com>
2026-03-16 09:56:30 -0500

    Update NixOS configuration on Mon Mar 16 09:56:30 AM CDT 2026 for gluon

--- dotfiles/fish/functions/windows.fish
+++ dotfiles/fish/functions/windows.fish
@@ -1,10 +1,12 @@
-function windows
-    # 1. Queue up Windows for the next boot in GRUB
+# This currently does not work, but I am not giving up on it
+
+function windows1
+    # Queue up Windows for the next boot in GRUB
     sudo grub-reboot 1
     
-    # 2. Tell the kernel's power management to reboot after saving state
+    # Tell the kernel's power management to reboot after saving state
     echo "reboot" | sudo tee /sys/power/disk > /dev/null
     
-    # 3. Trigger hibernation directly via the kernel
+    # Trigger hibernation directly via the kernel
     echo "disk" | sudo tee /sys/power/state > /dev/null
 end
