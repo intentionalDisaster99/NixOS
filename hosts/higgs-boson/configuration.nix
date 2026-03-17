c87eda760b955ca2a8ca199574b96f86105526cc
Sa9m <abyssalflerken@gmail.com>
2026-03-17 17:28:06 -0500

    Update NixOS configuration on Tue Mar 17 05:28:06 PM CDT 2026 for higgs-boson

--- hosts/higgs-boson/configuration.nix
+++ hosts/higgs-boson/configuration.nix
@@ -28,7 +28,7 @@
     ../../modules/rgb/openrgb.nix
     ../../modules/emacs/emacs.nix
     ../../modules/random-cool-stuff/alien.nix
-    ../../modules/school/mathematica.nix
+    # ../../modules/school/mathematica.nix # No longer needed
     ../../modules/gaming/moonlight.nix
     # ../../modules/keyboard/steno.nix # Removed because I could lock my computer with it on and not be able to sign in 
 
