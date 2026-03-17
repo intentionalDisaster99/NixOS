b63b5adc7f18c957598f5ce6feb2ef3998cfc031
Sa9m <abyssalflerken@gmail.com>
2026-03-17 17:29:47 -0500

    Update NixOS configuration on Tue Mar 17 05:29:47 PM CDT 2026 for higgs-boson

--- home.nix
+++ home.nix
@@ -107,6 +107,16 @@
     };
   };
 
-
+  # TODO Move this to its own module
+  # Where to save screenshots
+  xdg.configFile."swappy/config".text = ''
+    [Default]
+    save_dir=/home/scw0035/Pictures/Screenshots
+    save_filename_format=screenshot-%Y%m%d-%H%M%S.png
+    show_panel=false
+    line_size=5
+    text_size=20
+    text_font=sans-serif
+  '';
 
 }
