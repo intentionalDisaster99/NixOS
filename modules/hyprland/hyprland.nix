d989fb5461e15ffc9967f6e1e601ae70b1d624b8
Sa9m <abyssalflerken@gmail.com>
2026-03-17 18:28:04 -0500

    Update NixOS configuration on Tue Mar 17 06:28:04 PM CDT 2026 for higgs-boson

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -10,7 +10,9 @@
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
   # Adding in the plugin for my desktop
-  environment.etc."hyprland/plugins/hyprsplit.so".source = "${pkgs.hyprlandPlugins.hyprsplit}/lib/libhyprsplit.so";
+  # environment.etc."hyprland/plugins/hyprsplit.so".source = "${pkgs.hyprlandPlugins.hyprsplit}/lib/libhyprsplit.so";
+  environment.etc."hyprland/plugins/hyprsplit.so".source = "${inputs.hyprsplit.packages.${pkgs.system}.hyprsplit}/lib/libhyprsplit.so";
+
 
   programs.hyprlock.enable = true;
   services.hypridle.enable = true;
