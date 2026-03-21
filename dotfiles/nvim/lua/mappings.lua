c7b32cc05fb4a953d3674d04c91b127aee96a3b8
Sam Whitlock <abyssalflerken@gmail.com>
2026-03-21 12:15:16 -0500

    Update NixOS configuration on Sat Mar 21 12:15:16 PM CDT 2026 for gluon

--- dotfiles/nvim/lua/mappings.lua
+++ dotfiles/nvim/lua/mappings.lua
@@ -8,3 +8,9 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Press <Space> + q + s to restore the session for the current directory
+map("n", "<leader>qs", function() require("persistence").load() end, { desc = "Load Session" })
+
+-- Press <Space> + q + l to restore the last session you were working on, regardless of directory
+map("n", "<leader>ql", function() require("persistence").load({ last = true }) end, { desc = "Load Last Session" })
--- dotfiles/nvim/lua/options.lua
+++ dotfiles/nvim/lua/options.lua
@@ -6,3 +6,13 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- Only load the session if you run `nvim` with no arguments
+    if vim.fn.argc() == 0 then
+      require("persistence").load()
+    end
+  end,
+  nested = true,
+})
