a986b1c6ff640036f4d6765e28e352a93269ad77
Sa9m <abyssalflerken@gmail.com>
2026-03-31 14:46:49 -0500

    Update NixOS configuration on Tue Mar 31 02:46:49 PM CDT 2026 for higgs-boson

--- scripts/nrs_remote.sh
+++ scripts/nrs_remote.sh
@@ -47,9 +47,9 @@ fi
 
 # Rebuild the system using the specified flake and hostname
 echo "Rebuilding the system..."
-nix shell nixpkgs#nh nixpkgs#nix-output-monitor -c nh os switch "$NIXOS_CONFIG_DIR" --hostname "$HOSTNAME" -- builders 'ssh:gluon-linux'
+# nix shell nixpkgs#nh nixpkgs#nix-output-monitor -c nh os switch "$NIXOS_CONFIG_DIR" --hostname "$HOSTNAME" -- builders 'ssh:gluon-linux'
 # nix run nixpkgs#nh -- os switch "$NIXOS_CONFIG_DIR" --hostname "$HOSTNAME"
-# sudo nixos-rebuild switch --flake "$NIXOS_CONFIG_DIR#$HOSTNAME" |& nix run nixpkgs#nix-output-monitor
+sudo nixos-rebuild switch --flake "$NIXOS_CONFIG_DIR#$HOSTNAME" --builders 'ssh:gluon-linux' |& nix run nixpkgs#nix-output-monitor
 # sudo nixos-rebuild switch --flake "$NIXOS_CONFIG_DIR#$HOSTNAME" 
 
 echo "System update complete!"
