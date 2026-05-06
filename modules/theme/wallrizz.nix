d45995c5d831fdb34eb7e3dcefb57ad0c42c82a6
Sa9m <abyssalflerken@gmail.com>
2026-05-06 13:54:00 -0500

    Update NixOS configuration on Wed May  6 01:54:00 PM CDT 2026 for higgs-boson

--- modules/theme/wallrizz.nix
+++ modules/theme/wallrizz.nix
@@ -43,6 +43,7 @@ in
     home.packages = [ wallrizzPkg ];
 
     # Symlinking to ~/.config/WallRizz/themeExtensionScripts/
-    xdg.configFile."WallRizz/themeExtensionScripts".source = cfg.scriptSource;
+    # xdg.configFile."WallRizz/themeExtensionScripts".source = cfg.scriptSource;
+    xdg.configFile."WallRizz/themeExtensionScripts".source = ./dotfiles/wallrizz/test_scripts;
   };
 }
