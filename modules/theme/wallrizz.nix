753c0cafd0f24663d260ee8d7f50ead4e371aa91
Sa9m <abyssalflerken@gmail.com>
2026-05-06 12:59:32 -0500

    Update NixOS configuration on Wed May  6 12:59:32 PM CDT 2026 for higgs-boson

--- modules/theme/wallrizz.nix
+++ modules/theme/wallrizz.nix
@@ -23,8 +23,8 @@ let
       tar -xzf $src -C $out/bin/
       mv $out/bin/WallRizz* $out/bin/wallrizz
       chmod +x $out/bin/wallrizz
-      # wrapProgram $out/bin/wallrizz \
-        # --add-flags "-d /etc/nixos/resources/wallpapers/"
+      wrapProgram $out/bin/wallrizz \
+        --add-flags "-d /etc/nixos/resources/wallpapers/"
     '';
   };
 
@@ -43,6 +43,6 @@ in
     home.packages = [ wallrizzPkg ];
 
     # Symlinking to ~/.config/WallRizz/themeExtensionScripts/
-    xdg.configFile."WallRizz/themeExtensionScripts".source = cfg.scriptSource;
+    # xdg.configFile."WallRizz/themeExtensionScripts".source = cfg.scriptSource;
   };
 }
