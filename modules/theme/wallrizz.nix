59ae07d34c4458c0a39bd5139ddc3c8fd0e4e92b
Sa9m <abyssalflerken@gmail.com>
2026-05-06 11:43:32 -0500

    Update NixOS configuration on Wed May  6 11:43:32 AM CDT 2026 for higgs-boson

--- modules/theme/wallrizz.nix
+++ modules/theme/wallrizz.nix
@@ -9,7 +9,7 @@ let
     version = "1.4.0";
 
     src = pkgs.fetchurl {
-      url = "https://github.com/5hubham5ingh/WallRizz/releases/download/v${version}/WallRizz-linux-amd64";
+      url = "https://github.com/5hubham5ingh/WallRizz/releases/download/v${version}/WallRizz-linux-86_64";
       sha256 = "sha256-P8HllDD3bEX0rIGEMqiNFCd7+I9uJowaSCFf/u/u1vk=";
     };
 
