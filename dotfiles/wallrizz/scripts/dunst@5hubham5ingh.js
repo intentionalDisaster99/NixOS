4c31aaca4759cdafed3909414e58d3bde2f2130a
Sa9m <abyssalflerken@gmail.com>
2026-05-06 20:13:33 -0500

    Update NixOS configuration on Wed May  6 08:13:33 PM CDT 2026 for higgs-boson

--- dotfiles/wallrizz/scripts/dunst@5hubham5ingh.js
+++ dotfiles/wallrizz/scripts/dunst@5hubham5ingh.js
@@ -1,104 +1,104 @@
-/*
- For:            dunst,https://dunst-project.org
- Author:         https://github.com/5hubham5ingh
- Version:        0.0.1
- Prerequisite:   Dunst must be running for the theme to take effect.
+// /*
+//  For:            dunst,https://dunst-project.org
+//  Author:         https://github.com/5hubham5ingh
+//  Version:        0.0.1
+//  Prerequisite:   Dunst must be running for the theme to take effect.
 
- Note: Remove all the theme colour option from the /.config/dunst/dunstrc,
-       as they might overwrite the generated theme.
- */
+//  Note: Remove all the theme colour option from the /.config/dunst/dunstrc,
+//        as they might overwrite the generated theme.
+//  */
 
-function generateTheme(colors, isDark) {
-  const sortedColors = colors.sort((a, b) => {
-    const la = Color(a).getLuminance();
-    const lb = Color(b).getLuminance();
-    return isDark ? la - lb : lb - la;
-  });
+// function generateTheme(colors, isDark) {
+//   const sortedColors = colors.sort((a, b) => {
+//     const la = Color(a).getLuminance();
+//     const lb = Color(b).getLuminance();
+//     return isDark ? la - lb : lb - la;
+//   });
 
-  const backgroundIndex = isDark ? 0 : sortedColors.length - 1;
-  const foregroundIndex = isDark ? sortedColors.length - 1 : 0;
+//   const backgroundIndex = isDark ? 0 : sortedColors.length - 1;
+//   const foregroundIndex = isDark ? sortedColors.length - 1 : 0;
 
-  const background = sortedColors[backgroundIndex];
-  const foreground = sortedColors[foregroundIndex];
+//   const background = sortedColors[backgroundIndex];
+//   const foreground = sortedColors[foregroundIndex];
 
-  const midIndex = Math.floor(sortedColors.length / 2);
-  const frameColor = sortedColors[midIndex];
-  const criticalColor = sortedColors[Math.floor(midIndex * 0.75)];
+//   const midIndex = Math.floor(sortedColors.length / 2);
+//   const frameColor = sortedColors[midIndex];
+//   const criticalColor = sortedColors[Math.floor(midIndex * 0.75)];
 
-  return {
-    background,
-    foreground,
-    frameColor,
-    criticalColor,
-  };
-}
+//   return {
+//     background,
+//     foreground,
+//     frameColor,
+//     criticalColor,
+//   };
+// }
 
-function getThemeConf(colours, isDark) {
-  const theme = generateTheme(colours, isDark);
+// function getThemeConf(colours, isDark) {
+//   const theme = generateTheme(colours, isDark);
 
-  const config = `
-# WallRiz theme
-[global]
-frame_color = "${theme.frameColor}"
-separator_color = frame
+//   const config = `
+// # WallRiz theme
+// [global]
+// frame_color = "${theme.frameColor}"
+// separator_color = frame
 
-[urgency_low]
-background = "${theme.background}"
-foreground = "${theme.foreground}"
+// [urgency_low]
+// background = "${theme.background}"
+// foreground = "${theme.foreground}"
 
-[urgency_normal]
-background = "${theme.background}"
-foreground = "${theme.foreground}"
+// [urgency_normal]
+// background = "${theme.background}"
+// foreground = "${theme.foreground}"
 
-[urgency_critical]
-background = "${theme.background}"
-foreground = "${theme.foreground}"
-frame_color = "${theme.criticalColor}"
-# end
-  `.trim();
+// [urgency_critical]
+// background = "${theme.background}"
+// foreground = "${theme.foreground}"
+// frame_color = "${theme.criticalColor}"
+// # end
+//   `.trim();
 
-  return config;
-}
+//   return config;
+// }
 
-function getDarkThemeConf(colours) {
-  return getThemeConf(colours, true);
-}
+// function getDarkThemeConf(colours) {
+//   return getThemeConf(colours, true);
+// }
 
-function getLightThemeConf(colours) {
-  return getThemeConf(colours, false);
-}
+// function getLightThemeConf(colours) {
+//   return getThemeConf(colours, false);
+// }
 
-function setTheme(newThemeConfigPath) {
-  const dunstConfigPath = HOME_DIR.concat("/.config/dunst/dunstrc");
-  const dunstOldConfig = STD.loadFile(dunstConfigPath);
-  const newThemeConfig = STD.loadFile(newThemeConfigPath);
-  let filterOut = false;
-  let updated = false;
-  const updatedConfig = dunstOldConfig.split("\n")
-    .filter((line) => {
-      if (line.includes("# WallRiz theme")) {
-        filterOut = true;
-        updated = true;
-        return false;
-      }
-      if (filterOut) {
-        if (line.includes("# end")) {
-          filterOut = false;
-          return false;
-        }
-        return false;
-      }
-      return true;
-    })
-    .join("\n") + "\n" + newThemeConfig;
+// function setTheme(newThemeConfigPath) {
+//   const dunstConfigPath = HOME_DIR.concat("/.config/dunst/dunstrc");
+//   const dunstOldConfig = STD.loadFile(dunstConfigPath);
+//   const newThemeConfig = STD.loadFile(newThemeConfigPath);
+//   let filterOut = false;
+//   let updated = false;
+//   const updatedConfig = dunstOldConfig.split("\n")
+//     .filter((line) => {
+//       if (line.includes("# WallRiz theme")) {
+//         filterOut = true;
+//         updated = true;
+//         return false;
+//       }
+//       if (filterOut) {
+//         if (line.includes("# end")) {
+//           filterOut = false;
+//           return false;
+//         }
+//         return false;
+//       }
+//       return true;
+//     })
+//     .join("\n") + "\n" + newThemeConfig;
 
-  if (!updated) dunstOldConfig.concat("\n", newThemeConfig);
-  const dunstConfig = STD.open(dunstConfigPath, "w+");
+//   if (!updated) dunstOldConfig.concat("\n", newThemeConfig);
+//   const dunstConfig = STD.open(dunstConfigPath, "w+");
 
-  dunstConfig.puts(updatedConfig);
-  dunstConfig.close();
+//   dunstConfig.puts(updatedConfig);
+//   dunstConfig.close();
 
-  OS.exec(["pkill", "dunst"]);
-}
+//   OS.exec(["pkill", "dunst"]);
+// }
 
-export { getDarkThemeConf, getLightThemeConf, setTheme };
+// export { getDarkThemeConf, getLightThemeConf, setTheme };
