7b1bbfda0d869e4897a0106642716a1feb7f815c
Sam Whitlock <abyssalflerken@gmail.com>
2026-05-19 14:46:38 -0500

    Huge rollback, screw updating everything all at once

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,11 +1,11 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+# if status is-interactive
+#     # Commands to run in interactive sessions can go here
 
-    # Going into tmux if not alrady 
-    if not set -q TMUX
-        exec tmux new-session -A -s main
-    end
-end
+#     # Going into tmux if not alrady 
+#     if not set -q TMUX
+#         exec tmux new-session -A -s main
+#     end
+# end
 
 alias cl="clear"
 alias rad="rad-tui"
