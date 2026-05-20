4d01ea4e9947818ec79db1963e0ca52c1c938cb3
Sam Whitlock <abyssalflerken@gmail.com>
2026-05-20 11:57:42 -0500

    Update NixOS configuration on Wed May 20 11:57:42 AM CDT 2026 for gluon

--- hosts/gluon/configuration.nix
+++ hosts/gluon/configuration.nix
@@ -25,6 +25,7 @@
     ../../modules/rgb/openrgb.nix
     ../../modules/brightness/pc-brightness.nix
     ../../modules/emacs/emacs.nix
+    ../../modules/smb/smb-client.nix
     ../../modules/random-cool-stuff/droidcam.nix
     # ../../modules/school/mathematica.nix # No longer neeeded
     ../../modules/remote/sunshine.nix # I got rid of this because it was not working and causing me to boot to kde instead of hyprland
