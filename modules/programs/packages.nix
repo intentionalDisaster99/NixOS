14cb0df78670aaabb4adbb8732227544d7206ab1
Sam Whitlock <abyssalflerken@gmail.com>
2026-06-02 14:25:01 -0500

    Update NixOS configuration on Tue Jun  2 02:25:01 PM CDT 2026 for gluon

--- modules/programs/packages.nix
+++ modules/programs/packages.nix
@@ -261,7 +261,7 @@
     pkgs.golazo
     # aewan # Technically could be quite cool, but I don't really want to put the effort into learning it
     # inputs.terminal-rain.packages.${stdenv.hostPlatform.system}.terminal-rain-lightning
-    pywal
+    pkgs.pywal
 
 
     # ---------------------------------------------------
