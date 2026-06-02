b2de55236f74016fa53543e46aeb40b7a4b2e745
Sam Whitlock <abyssalflerken@gmail.com>
2026-06-02 14:23:00 -0500

    Update NixOS configuration on Tue Jun  2 02:23:00 PM CDT 2026 for gluon

--- modules/programs/packages.nix
+++ modules/programs/packages.nix
@@ -2,262 +2,263 @@
 { config, pkgs, pkgs-latest, lib, inputs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
+  environment.systemPackages = [
 
     # ---------------------------------------------------
     # OS Utils 
     # ---------------------------------------------------
-    grub2
-    os-prober
-    networkmanager
-    gtk4
-    polkit_gnome
-    gsettings-desktop-schemas
-    glib-networking
-    gvfs
-    upower
-    xdg-desktop-portal
-    xdg-desktop-portal-gtk
-    nixpkgs-fmt
-    nix-output-monitor
-    nh
+    pkgs.grub2
+    pkgs.os-prober
+    pkgs.networkmanager
+    pkgs.gtk4
+    pkgs.polkit_gnome
+    pkgs.gsettings-desktop-schemas
+    pkgs.glib-networking
+    pkgs.gvfs
+    pkgs.upower
+    pkgs.xdg-desktop-portal
+    pkgs.xdg-desktop-portal-gtk
+    pkgs.nixpkgs-fmt
+    pkgs.nix-output-monitor
+    pkgs.nh
 
     # ---------------------------------------------------
     # Hyprland
-    # ---------------------------------------------------
-    eww
-    quickshell
-    waybar
-    rofi
-    wlogout
-    hyprpaper
-    swaylock-fancy
-    dunst
-    libnotify
-    mpvpaper
-    avizo
-    unclutter
-    kdePackages.qt6ct
-    kdePackages.breeze
-    libsForQt5.qt5ct
+    pkgs.# ---------------------------------------------------
+    pkgs.eww
+    pkgs.quickshell
+    pkgs.waybar
+    pkgs.rofi
+    pkgs.wlogout
+    pkgs.hyprpaper
+    pkgs.swaylock-fancy
+    pkgs.dunst
+    pkgs.libnotify
+    pkgs.mpvpaper
+    pkgs.avizo
+    pkgs.unclutter
+    pkgs.kdePackages.qt6ct
+    pkgs.kdePackages.breeze
+    pkgs.libsForQt5.qt5ct
 
     # Screen capture & clipboards
-    grim
-    slurp
-    swappy
-    eyedropper
-    wl-clipboard
-    cliphist
-    wtype
-    jq
+    pkgs.grim
+    pkgs.slurp
+    pkgs.swappy
+    pkgs.eyedropper
+    pkgs.wl-clipboard
+    pkgs.cliphist
+    pkgs.wtype
+    pkgs.jq
 
     # Audio & Display control
-    pipewire
-    wireplumber
-    pavucontrol
-    playerctl
-    brightnessctl
-    ffmpeg_6
+    pkgs.pipewire
+    pkgs.wireplumber
+    pkgs.pavucontrol
+    pkgs.playerctl
+    pkgs.brightnessctl
+    pkgs.ffmpeg_6
 
     # GUI widget/script dependencies (Crucial for eww/quickshell!)
-    jq
-    socat
-    pamixer
-    acpi
-    iw
-    bluez
-    bc
-    pulseaudio
-    imagemagick
+    pkgs.jq
+    pkgs.socat
+    pkgs.pamixer
+    pkgs.acpi
+    pkgs.iw
+    pkgs.bluez
+    pkgs.bc
+    pkgs.pulseaudio
+    pkgs.imagemagick
 
     # ---------------------------------------------------
     # Terminal stuff
     # ---------------------------------------------------
-    fish
-    starship
-    kitty
-    kdePackages.yakuake
-    zoxide
-    fastfetch
-    hyfetch
-    lsd
-    eza
-    bat
-    fzf
-    ripgrep
-    tldr
-    dust
-    lf
-    psmisc
-    parallel
-    atuin
-    direnv
-    mise
-    wget
+    pkgs.fish
+    pkgs.starship
+    pkgs.kitty
+    pkgs.kdePackages.yakuake
+    pkgs.zoxide
+    pkgs.fastfetch
+    pkgs.hyfetch
+    pkgs.lsd
+    pkgs.eza
+    pkgs.bat
+    pkgs.fzf
+    pkgs.ripgrep
+    pkgs.tldr
+    pkgs.dust
+    pkgs.lf
+    pkgs.psmisc
+    pkgs.parallel
+    pkgs.atuin
+    pkgs.direnv
+    pkgs.mise
+    pkgs.wget
 
     # ---------------------------------------------------
     # Dev
     # ---------------------------------------------------
-    vim
-    nano
-    neovim
-    vscode
-    vscodium-fhs
-    arduino-ide
-    platformio
-    jdk
-    tmux
-    # netbeans
-    python3
-    gcc
-    gdb
-    gef
-    git
-    github-desktop
-    lazygit
-    radicle-tui
-    curl
+    pkgs.vim
+    pkgs.nano
+    pkgs.neovim
+    pkgs.vscode
+    pkgs.vscodium-fhs
+    pkgs.arduino-ide
+    pkgs.platformio
+    pkgs.jdk
+    pkgs.tmux
+    # pkgs.netbeans
+    pkgs.python3
+    pkgs.gcc
+    pkgs.gdb
+    pkgs.gef
+    pkgs.git
+    pkgs.github-desktop
+    pkgs.lazygit
+    pkgs.radicle-tui
+    pkgs.curl
 
     # Rust specific
-    cargo
-    rustup
+    pkgs.cargo
+    pkgs.rustup
 
     # ---------------------------------------------------
     # Embedded stuff 
     # ---------------------------------------------------
-    picotool
-    probe-rs-tools
-    pico-sdk
-    python313Packages.cmake
-    gnumake42
-    cargo-generate
-    tio
+    pkgs.picotool
+    pkgs.probe-rs-tools
+    pkgs.pico-sdk
+    pkgs.python313Packages.cmake
+    pkgs.gnumake42
+    pkgs.cargo-generate
+    pkgs.tio
 
     # ---------------------------------------------------
     # Server and utilities things
     # ---------------------------------------------------
-    docker
-    docker-compose
-    virt-manager
-    qemu
-    freerdp
-    openssh
-    sshfs
-    cifs-utils
-    syncthing
-    rclone
-    google-drive-ocamlfuse
-    grsync
-    gparted
-    kdePackages.filelight
-    usbutils
-    ventoy-full
-    wine
-    bottles
-    swtpm
-    firejail
-    winboat
-    waypipe
-    comma
+    pkgs.docker
+    pkgs.docker-compose
+    pkgs.virt-manager
+    pkgs.qemu
+    pkgs.freerdp
+    pkgs.openssh
+    pkgs.sshfs
+    pkgs.cifs-utils
+    pkgs.syncthing
+    pkgs.rclone
+    pkgs.google-drive-ocamlfuse
+    pkgs.grsync
+    pkgs.gparted
+    pkgs.kdePackages.filelight
+    pkgs.usbutils
+    pkgs.ventoy-full
+    pkgs.wine
+    pkgs.bottles
+    pkgs.swtpm
+    pkgs.firejail
+    pkgs.winboat
+    pkgs.waypipe
+    pkgs.comma
 
     # System monitoring
-    btop-cuda
-    htop
-    s-tui
-    powertop
-    cpu-x
-    tlp
-    lm_sensors
-    sysprof
+    pkgs.btop-cuda
+    pkgs.htop
+    pkgs.s-tui
+    pkgs.powertop
+    pkgs.cpu-x
+    pkgs.tlp
+    pkgs.lm_sensors
+    pkgs.sysprof
 
     # Networking
-    geteduroam
-    tailscale
-    wgnord
-    openvpn
-    networkmanagerapplet
-    blueman # backup bluetooth manager `blueman-manager`
-    overskride
+    pkgs.geteduroam
+    pkgs.tailscale
+    pkgs.wgnord
+    pkgs.openvpn
+    pkgs.networkmanagerapplet
+    pkgs.blueman # backup bluetooth manager `blueman-manager`
+    pkgs.overskride
 
     # ---------------------------------------------------
     # General Apps
     # ---------------------------------------------------
-    brave
-    spotify
-    discord
-    element-desktop
-    zoom-us
-    obs-studio
-    davinci-resolve
-    kdePackages.kdenlive
-    vlc
-    blender
-    cheese
-    nautilus
-    gnome-control-center
-    gnome-online-accounts
-    seahorse
-    wofi-emoji
-    emote
-    qalculate-gtk
-    kdePackages.gwenview
-    kdePackages.dolphin
-    kdePackages.dolphin-plugins
-    desktop-file-utils
-    kdePackages.plasma-workspace
-    kdePackages.ark
-    kdePackages.kio
-    kdePackages.kio-extras
-    xdg-utils
-    shared-mime-info
-    kdePackages.qtstyleplugin-kvantum
-    libsForQt5.qtstyleplugin-kvantum
-    gruvbox-dark-gtk
-    gruvbox-plus-icons
+    pkgs.brave
+    pkgs.spotify
+    pkgs.discord
+    pkgs.element-desktop
+    pkgs.zoom-us
+    pkgs-latest.rustdesk-flutter
+    pkgs.obs-studio
+    pkgs.davinci-resolve
+    pkgs.kdePackages.kdenlive
+    pkgs.vlc
+    pkgs.blender
+    pkgs.cheese
+    pkgs.nautilus
+    pkgs.gnome-control-center
+    pkgs.gnome-online-accounts
+    pkgs.seahorse
+    pkgs.wofi-emoji
+    pkgs.emote
+    pkgs.qalculate-gtk
+    pkgs.kdePackages.gwenview
+    pkgs.kdePackages.dolphin
+    pkgs.kdePackages.dolphin-plugins
+    pkgs.desktop-file-utils
+    pkgs.kdePackages.plasma-workspace
+    pkgs.kdePackages.ark
+    pkgs.kdePackages.kio
+    pkgs.kdePackages.kio-extras
+    pkgs.xdg-utils
+    pkgs.shared-mime-info
+    pkgs.kdePackages.qtstyleplugin-kvantum
+    pkgs.libsForQt5.qtstyleplugin-kvantum
+    pkgs.gruvbox-dark-gtk
+    pkgs.gruvbox-plus-icons
 
     # ---------------------------------------------------
     # School
     # ---------------------------------------------------
-    obsidian
-    libreoffice-qt6-fresh
-    texliveFull
-    pandoc
-    drawio
-    tesseract4
-    gImageReader
-    ocrmypdf
+    pkgs.obsidian
+    pkgs.libreoffice-qt6-fresh
+    pkgs.texliveFull
+    pkgs.pandoc
+    pkgs.drawio
+    pkgs.tesseract4
+    pkgs.gImageReader
+    pkgs.ocrmypdf
 
     # ---------------------------------------------------
     # Electrical
     # ---------------------------------------------------
-    kicad
+    pkgs.kicad
 
     # ---------------------------------------------------
     # Gaming
     # ---------------------------------------------------
-    steam
-    prismlauncher
+    pkgs.steam
+    pkgs.prismlauncher
 
     # ---------------------------------------------------
     # Silly Things
     # ---------------------------------------------------
-    activate-linux
-    kittysay
-    neo-cowsay
-    fortune
-    sl
-    pay-respects
-    gping
-    fireplace
-    figlet
-    mapscii
-    nyancat
-    cbonsai
-    asciiquarium
-    xcowsay
-    pipes
-    lolcat
-    golazo
+    pkgs.activate-linux
+    pkgs.kittysay
+    pkgs.neo-cowsay
+    pkgs.fortune
+    pkgs.sl
+    pkgs.pay-respects
+    pkgs.gping
+    pkgs.fireplace
+    pkgs.figlet
+    pkgs.mapscii
+    pkgs.nyancat
+    pkgs.cbonsai
+    pkgs.asciiquarium
+    pkgs.xcowsay
+    pkgs.pipes
+    pkgs.lolcat
+    pkgs.golazo
     # aewan # Technically could be quite cool, but I don't really want to put the effort into learning it
     inputs.terminal-rain.packages.${stdenv.hostPlatform.system}.terminal-rain-lightning
     pywal
@@ -268,17 +269,11 @@
     # ---------------------------------------------------
     # mathematica
     # blender
-    jetbrains.idea
+    pkgs.jetbrains.idea
     # logisim-evolution
     # quartus-prime-lite
     # kdewalletmanager
     # kdePackages.kwallet
   ];
 
-  # Packages that I want to keep on the bleeding edge 
-  environment.systemPackages = with pkges-latest; [
-    # Keeping RustDesk updated so that it actually works
-    rustdesk-flutter
-
-  ];
 }
