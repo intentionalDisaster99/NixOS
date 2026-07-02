2353ee1ab2f0258f9c513517982a184815961542
Sa9m <abyssalflerken@gmail.com>
2026-07-01 22:01:10 -0500

    Update NixOS configuration on Wed Jul  1 10:01:10 PM CDT 2026 for higgs-boson

--- Modules/KDE-Connect/home.nix
+++ Modules/KDE-Connect/home.nix
@@ -1,11 +1,12 @@
 # Installs and enables KDE connect so that I can connect with ma phone
 { users, config, pkgs, inputs, ... }:
 {
-  services.kdeconnect.enable = true;
 
-  networking.firewall = rec {
-    allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
-    allowedUDPPortRanges = allowedTCPPortRanges;
-  };
+  home-manager.users.sa9m.services.kdeconnect.enable = true;
+
+  # networking.firewall = rec {
+  #   allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
+  #   allowedUDPPortRanges = allowedTCPPortRanges;
+  # };
 
 }
