69a89a4734f75885e65adb47820fc708c3a509f8
Sa9m <abyssalflerken@gmail.com>
2026-07-01 22:03:20 -0500

    Update NixOS configuration on Wed Jul  1 10:03:20 PM CDT 2026 for higgs-boson

--- Modules/KDE-Connect/home.nix
+++ Modules/KDE-Connect/home.nix
@@ -4,9 +4,14 @@
 
   services.kdeconnect.enable = true;
 
-  networking.firewall = rec {
-    allowedTCPPortRanges = [{ from = 1714; to = 1764; }];
-    allowedUDPPortRanges = allowedTCPPortRanges;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPortRanges = [
+      { from = 1714; to = 1764; } # KDE Connect
+    ];
+    allowedUDPPortRanges = [
+      { from = 1714; to = 1764; } # KDE Connect
+    ];
   };
 
 }
