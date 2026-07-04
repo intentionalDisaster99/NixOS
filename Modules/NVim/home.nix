1bf89a52e2c23688621b6314966546b5feb3cfdf
Sa9m <abyssalflerken@gmail.com>
2026-07-04 16:46:48 -0500

    Update NixOS configuration on Sat Jul  4 04:46:48 PM CDT 2026 for higgs-boson

--- Modules/NVim/home.nix
+++ Modules/NVim/home.nix
@@ -2,19 +2,19 @@
 { config, pkgs, ... }:
 
 {
-  programs.neovim = {
-    # enable = true;
-    defaultEditor = true;
-    extraPackages = with pkgs; [
-      ripgrep
-      gcc
-      gnumake
-      unzip
-      wl-clipboard
-      nodejs
-      tree-sitter
-    ];
-  };
+
+
+  home.packages = with pkgs; [
+    neovim
+    ripgrep
+    gcc
+    gnumake
+    unzip
+    wl-clipboard
+    nodejs
+    tree-sitter
+  ];
+
 
   # Symlinking to my dots
   xdg.configFile."nvim".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/Modules/NVim/Dots";
