636f6edf624e9285ac6bc882e1f23b927fd9004a
Sa9m <abyssalflerken@gmail.com>
2026-07-04 16:08:48 -0500

    Update NixOS configuration on Sat Jul  4 04:08:48 PM CDT 2026 for higgs-boson

--- Modules/NVim/Dots/lua/configs/lspconfig.lua
+++
@@ -1,7 +0,0 @@
-require("nvchad.configs.lspconfig").defaults()
-
-local servers = { "html", "cssls", "codebook", "cpptools", "nixfmt", "nixpkgs-fmt", "rust-analyzer", "snyk" }
-
-vim.lsp.enable(servers)
-
--- read :h vim.lsp.config for changing options of lsp servers 
