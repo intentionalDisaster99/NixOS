4ef3fbe33564e9ad1cf9a0c7aca224dade43194a
Sa9m <abyssalflerken@gmail.com>
2026-07-05 15:45:19 -0500

    Update NixOS configuration on Sun Jul  5 03:45:19 PM CDT 2026 for higgs-boson

--- Modules/Kitty/home.nix
+++ Modules/Kitty/home.nix
@@ -7,16 +7,21 @@
   home.packages = with pkgs; [
     kitty
   ];
-  programs.kitty = {
-    enable = true;
 
-    # Enable native Fish integration
-    shellIntegration.enableFishIntegration = true;
 
-    # # Explicitly set the shell Kitty should launch
-    settings = {
-      shell = "${pkgs.fish}/bin/fish";
-    };
-  };
+  xdg.configFile."kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/Modules/Kitty/Dots/kitty.conf";
+
+  # If you wanted to use home-manager to generate dots, but I want to use my own
+  # programs.kitty = {
+  #   enable = true;
+  #
+  #   # Enable native Fish integration
+  #   shellIntegration.enableFishIntegration = true;
+  #
+  #   # # Explicitly set the shell Kitty should launch
+  #   settings = {
+  #     shell = "${pkgs.fish}/bin/fish";
+  #   };
+  # };
 
 }
