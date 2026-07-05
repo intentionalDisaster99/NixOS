3484d5fcba505b06d3fc9745639bb18847ddb13b
Sa9m <abyssalflerken@gmail.com>
2026-07-04 23:55:18 -0500

    Update NixOS configuration on Sat Jul  4 11:55:18 PM CDT 2026 for higgs-boson

--- Modules/NVim/Dots/init.lua
+++ Modules/NVim/Dots/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- For noctalia (no idea if this is how you are meant to do this)
+require("colors.noctalia")
---
+++ Modules/NVim/Dots/lua/colors/noctalia.lua
@@ -0,0 +1,7 @@
+return {
+  bg = "#181211",
+  fg = "#ede0dd",
+  accent = "#ffb4a7",
+  error = "#ffb4ab",
+  warning = "#ddc48c",
+}
