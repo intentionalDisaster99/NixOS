b04f4419be85f991cc17725fbd42e5df7e4e4807
Sam Whitlock <abyssalflerken@gmail.com>
2026-07-27 00:03:33 -0500

    Update NixOS configuration on Mon Jul 27 12:03:33 AM CDT 2026 for gluon

--- Resources/Profile/profile.nix
+++ Resources/Profile/profile.nix
@@ -1,7 +1,7 @@
 # Home-manager module to symlink my profile stuff to the places they are expected 
 { pkgs, hyprland, config, inputs, username, ... }: {
 
-  home-manager.users.${username} = {
+  home-manager.users.${username} = { config, ... }: {
     home.file.".face" = {
       source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/Resources/Profile/profile.jpg";
     };
