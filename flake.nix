c0f6bf33be4cda7d312f2b22735fdad67c5dc687
Sa9m <abyssalflerken@gmail.com>
2026-07-29 22:52:37 -0500

    Update NixOS configuration on Wed Jul 29 10:52:37 PM CDT 2026 for higgs-boson

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia-dev/noctalia";
+      url = "github:noctalia-dev/noctalia";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland.url = "github:hyprwm/Hyprland";
