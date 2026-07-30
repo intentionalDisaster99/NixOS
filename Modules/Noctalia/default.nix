7ab479d0d4b87ae1bf52e9d6879c8fbadeb93870
Sa9m <abyssalflerken@gmail.com>
2026-07-29 23:02:19 -0500

    Update NixOS configuration on Wed Jul 29 11:02:19 PM CDT 2026 for higgs-boson

--- Modules/Noctalia/default.nix
+++ Modules/Noctalia/default.nix
@@ -17,19 +17,6 @@
       programs.noctalia = {
         enable = true;
 
-        settings = {
-          # This may also be a string or path to a .toml file.
-          theme = {
-            mode = "dark";
-            source = "builtin";
-            builtin = "Catppuccin";
-          };
-
-          wallpaper = {
-            enabled = true;
-            default.path = "/path/to/wallpapers/wallpaper.png";
-          };
-        };
         # Symlinking to my dots
         home.file.".config/noctalia" = {
           source = config.lib.file.mkOutOfStoreSymlink "/etc/nixos/Modules/Noctalia/Dots";
