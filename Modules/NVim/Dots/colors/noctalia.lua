574ed63272bf68a378a4adf43c491f41121f74b4
Sa9m <abyssalflerken@gmail.com>
2026-08-14 22:04:46 -0500

    Update NixOS configuration on Fri Aug 14 10:04:46 PM CDT 2026 for higgs-boson

--- Modules/NVim/Dots/colors/noctalia.lua
+++ Modules/NVim/Dots/colors/noctalia.lua
@@ -11,24 +11,24 @@ end
 
 hl("Normal",       { fg = "#e4e1e6", bg = "#131316" })
 hl("NormalFloat",  { fg = "#e4e1e6", bg = "#131316" })
-hl("Cursor",       { fg = "#131316", bg = "#bcc3ff" })
+hl("Cursor",       { fg = "#131316", bg = "#b8c3ff" })
 
 hl("CursorLine",   { bg = "#2a2a2d" })
 hl("LineNr",       { fg = "#90909a" })
-hl("CursorLineNr", { fg = "#bcc3ff", bold = true })
+hl("CursorLineNr", { fg = "#b8c3ff", bold = true })
 
 hl("Comment",      { fg = "#90909a", italic = true })
-hl("Keyword",      { fg = "#bcc3ff", bold = true })
-hl("Function",     { fg = "#bcc3ff" })
+hl("Keyword",      { fg = "#b8c3ff", bold = true })
+hl("Function",     { fg = "#b8c3ff" })
 hl("String",       { fg = "#e4e1e6" })
 hl("Identifier",   { fg = "#e4e1e6" })
-hl("Constant",     { fg = "#c4c5dd" })
+hl("Constant",     { fg = "#c3c5dd" })
 
 hl("DiagnosticError", { fg = "#ffb4ab" })
-hl("DiagnosticWarn",  { fg = "#e6bad7" })
-hl("DiagnosticInfo",  { fg = "#bcc3ff" })
+hl("DiagnosticWarn",  { fg = "#e4bad9" })
+hl("DiagnosticInfo",  { fg = "#b8c3ff" })
 hl("DiagnosticHint",  { fg = "#e4e1e6" })
 
-hl("WinSeparator", { fg = "#bcc3ff" })
+hl("WinSeparator", { fg = "#b8c3ff" })
 hl("Pmenu",        { fg = "#e4e1e6", bg = "#1f1f23" })
-hl("PmenuSel",     { fg = "#131316", bg = "#bcc3ff" })
+hl("PmenuSel",     { fg = "#131316", bg = "#b8c3ff" })
