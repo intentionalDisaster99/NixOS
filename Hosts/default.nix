4a33a6fb07355b635c5d47800faa5efadcae04bd
Sa9m <abyssalflerken@gmail.com>
2026-09-09 16:44:08 -0500

    Update NixOS configuration on Wed Sep  9 04:44:08 PM CDT 2026 for higgs-boson

--- Hosts/default.nix
+++ Hosts/default.nix
@@ -137,6 +137,8 @@
         pavucontrol # This will likely get moved to hyprland so that I can have it automatically pop up in a special workspace when I want it
         overskride # Same thing with things like bluetooth, which I believe is like Overskride
 
+        arduino
+
         # TODO move
         kicad
         github-desktop
